6b62c9d4153213666b8e9d2579c7e965dcc9f630
Pol Gubau Amores <gubaupol@gmail.com>
2023-08-31 09:17:44 +0200

    Pol Gubau; MW-Correcting tests

--- src/components/Layout/Tabs/Tabs.tsx
+++ src/components/Layout/Tabs/Tabs.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useMemo } from "react";
 import TabBar from "./components/TabBar/TabBar";
 import { applyMaxWidth, applyPadding, applyRounded } from "../../../style";
--- src/utils/icons/getIcon.tsx
+++ src/utils/icons/getIcon.tsx
@@ -1,7 +1,8 @@
 import { IconData } from "../../components";
 import icons from "../../model/icons.model";
 
-export const getIcon = (name: string, iconGroup: IconData[] = icons) => {
-	const icon = iconGroup.find((icon) => icon.name.toLowerCase() === name.toLowerCase());
+export const getIcon = (name: string, iconList?: IconData[]) => {
+	const allIcons = iconList ?? icons;
+	const icon = allIcons.find((icon) => icon.name.toLowerCase() === name.toLowerCase());
 	return icon?.icon ?? null;
 };
