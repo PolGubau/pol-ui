482f8ece68a7903a84280be0795b7444d7d7450c
Pol Gubau Amores <gubaupol@gmail.com>
2023-09-17 11:55:09 +0200

    Pol Gubau; MW-New css colors to customize package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 
-const palette = {
-	white: "#FAFAFA",
-	black: "#232323",
-
-	primary: "#2a2945",
-	primaryDark: "#8986db",
-
-	secondary: "#435db0",
-	secondaryDark: "#bcbbeb",
-
-	accent: "#ffb300",
-	accentDark: "#dfe65f",
-
-	success: "#9bc88e",
-	successDark: "#9bc88e",
-
-	danger: "#ff7b7b",
-	dangerDark: "#ff7b7b",
-
-	info: "#56caca",
-	infoDark: "#56caca",
-};
-
 const config = {
 	darkMode: "class",
 
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {},
+	theme: {
+		colors: {
+			background: { DEFAULT: "var(--theme-white)", dark: "var(--theme-black)" },
+			contrast: { DEFAULT: "var(--theme-black)", dark: "var(--theme-white)" },
+			// Base
+			primary: { DEFAULT: "var(--theme-primary)", dark: "var(--theme-primary-dark)" },
+			secondary: { DEFAULT: "var(--theme-secondary)", dark: "var(--theme-secondary-dark)" },
+			// Accent
+			accent: { DEFAULT: "var(--theme-accent)", dark: "var(--theme-accent-dark)" },
+			// Status
+			success: { DEFAULT: "var(--theme-success)", dark: "var(--theme-success-dark)" },
+			danger: { DEFAULT: "var(--theme-danger)", dark: "var(--theme-danger-dark)" },
+			info: { DEFAULT: "var(--theme-info)", dark: "var(--theme-info-dark)" },
+			// Utility
+			transparent: "transparent",
+		},
+	},
 
 	plugins: [require("tailwindcss-animated"), require("@headlessui/tailwindcss")],
 };
