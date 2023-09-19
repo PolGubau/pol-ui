05764398596966955b9f2ad67c78196ef5339ab3
Pol Gubau Amores <gubaupol@gmail.com>
2023-09-19 08:42:42 +0200

    Pol Gubau; MW-useMediaQuery hook

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const useMediaQuery = (width: number) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: { matches: any }) => {
+    if (e.matches) setTargetReached(true);
+    else setTargetReached(false);
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addEventListener("change", updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) setTargetReached(true);
+
+    return () => media.removeEventListener("change", updateTarget);
+  }, [updateTarget, width]);
+
+  return targetReached;
+};
