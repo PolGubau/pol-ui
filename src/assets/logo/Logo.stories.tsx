ba524ea78abd029dede0fdaf72b1f9321ed2930d
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-31 16:13:27 +0100

    Pol Gubau; MW-Welcome message

---
+++ src/assets/logo/Logo.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Logo from "./Logo";
+import { Link, Text } from "../../components";
+
+const meta = {
+	title: "Welcome",
+	component: Logo,
+	tags: ["autodocs"],
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	render: (args) => (
+		<>
+			<Text as="h1">Hello</Text>
+			<Text>
+				You'll find here some examples of Pol/ui potential, have a great time and contact us in case
+				of doubts.
+			</Text>
+			<Link href="mailto:gubaupol@gmail.com" className="underline mt-8">
+				Author's email
+			</Link>
+		</>
+	),
+};
