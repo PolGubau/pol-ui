87777dac3d11c8467b75b1944e5a553eb3f11713
Pol Gubau <gubaupol@gmail.com>
2024-01-14 14:25:42 +0100

    Add lint-fix script and reformat code

--- src/components/Avatar/index.ts
+++ src/components/Avatar/index.ts
@@ -1,16 +1,14 @@
-export { Avatar } from './Avatar';
+export { Avatar } from './Avatar'
 export type {
-  AvatarColors,
   AvatarImageProps,
   AvatarProps,
   AvatarSizes,
   AvatarImageTheme,
   AvatarInitialsTheme,
   AvatarRootTheme,
-  AvatarStatusTheme,
   AvatarTheme,
-} from './Avatar';
-export { AvatarGroup } from './AvatarGroup';
-export type { AvatarGroupProps, AvatarGroupTheme } from './AvatarGroup';
-export { AvatarGroupCounter } from './AvatarGroupCounter';
-export type { AvatarGroupCounterProps, AvatarGroupCounterTheme } from './AvatarGroupCounter';
+} from './Avatar'
+export { AvatarGroup } from './AvatarGroup'
+export type { AvatarGroupProps, AvatarGroupTheme } from './AvatarGroup'
+export { AvatarGroupCounter } from './AvatarGroupCounter'
+export type { AvatarGroupCounterProps, AvatarGroupCounterTheme } from './AvatarGroupCounter'
