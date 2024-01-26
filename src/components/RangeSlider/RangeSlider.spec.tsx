4e76c7eb66f36a208947a3ea0af4d6f0a5c54d27
Pol Gubau <gubaupol@gmail.com>
2024-01-26 20:46:50 +0100

    Refactor code for improved performance and readability

--- src/components/RangeSlider/RangeSlider.spec.tsx
+++ src/components/RangeSlider/RangeSlider.spec.tsx
@@ -1,90 +1,90 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createRef } from 'react';
-import { describe, expect, it, vi } from 'vitest';
-import { PoluiProvider, type CustomPoluiTheme } from '../PoluiProvider';
-import { RangeSlider } from './RangeSlider';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createRef } from 'react'
+import { describe, expect, it, vi } from 'vitest'
+import { PoluiProvider, type CustomPoluiTheme } from '../PoluiProvider'
+import { RangeSlider } from './RangeSlider'
 
 describe('Components / Button', () => {
   describe('A11y', () => {
     it('should have `role="progressbar"` by default', () => {
-      render(<RangeSlider />);
+      render(<RangeSlider />)
 
-      expect(rangeSlider()).toBeInTheDocument();
-    });
+      expect(rangeSlider()).toBeInTheDocument()
+    })
 
     it('should be able to use any other role permitted for `RangeSlider`', () => {
       // eslint-disable-next-line jsx-a11y/aria-role
-      render(<RangeSlider role="rangeinput" />);
+      render(<RangeSlider role="rangeinput" />)
 
-      expect(rangeSlider('rangeinput')).toBeInTheDocument();
-    });
-  });
+      expect(rangeSlider('rangeinput')).toBeInTheDocument()
+    })
+  })
 
   describe('Keyboard interactions', () => {
     it('should focus when `Tab` is pressed', async () => {
-      const user = userEvent.setup();
-      render(<RangeSlider />);
+      const user = userEvent.setup()
+      render(<RangeSlider />)
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSlider()).toHaveFocus();
-    });
+      expect(rangeSlider()).toHaveFocus()
+    })
 
     it('should be possible to `Tab` out', async () => {
-      const user = userEvent.setup();
+      const user = userEvent.setup()
       render(
         <>
           <RangeSlider />
           <RangeSlider />
           <RangeSlider />
         </>,
-      );
+      )
 
-      const rangeSliderElements = rangeSliders();
+      const rangeSliderElements = rangeSliders()
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSliderElements[0]).toHaveFocus();
+      expect(rangeSliderElements[0]).toHaveFocus()
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSliderElements[1]).toHaveFocus();
+      expect(rangeSliderElements[1]).toHaveFocus()
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSliderElements[2]).toHaveFocus();
-    });
+      expect(rangeSliderElements[2]).toHaveFocus()
+    })
 
     it('should not trigger `onChange` when `Space` is pressed', async () => {
-      const user = userEvent.setup();
-      const handleChange = vi.fn();
+      const user = userEvent.setup()
+      const handleChange = vi.fn()
 
-      render(<RangeSlider onChange={handleChange} />);
+      render(<RangeSlider onChange={handleChange} />)
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSlider()).toHaveFocus();
+      expect(rangeSlider()).toHaveFocus()
 
-      await user.keyboard('[Space]');
+      await user.keyboard('[Space]')
 
-      expect(handleChange).not.toHaveBeenCalled();
-    });
+      expect(handleChange).not.toHaveBeenCalled()
+    })
 
     it('should not trigger `onChange` when `Enter` is pressed', async () => {
-      const user = userEvent.setup();
-      const handleChange = vi.fn();
+      const user = userEvent.setup()
+      const handleChange = vi.fn()
 
-      render(<RangeSlider onChange={handleChange} />);
+      render(<RangeSlider onChange={handleChange} />)
 
-      await user.tab();
+      await user.tab()
 
-      expect(rangeSlider()).toHaveFocus();
+      expect(rangeSlider()).toHaveFocus()
 
-      await user.keyboard('[Enter]');
+      await user.keyboard('[Enter]')
 
-      expect(handleChange).not.toHaveBeenCalled();
-    });
+      expect(handleChange).not.toHaveBeenCalled()
+    })
 
     /**
      * Test Name: Should trigger `onChange` when `Arrow` key pressed
@@ -99,42 +99,42 @@ describe('Components / Button', () => {
      * TODO: Once these issues get fixed, we will add this test case.
      *
      */
-  });
+  })
 
   describe('Props', () => {
     it('should allow HTML attributes for `<input type="range">`s', () => {
-      render(<RangeSlider formAction="post.php" min={4} max={10} step={0.5} />);
-      const rangeSliderElement = rangeSlider();
-      expect(rangeSliderElement).toHaveAttribute('formAction', 'post.php');
-      expect(rangeSliderElement).toHaveAttribute('min', '4');
-      expect(rangeSliderElement).toHaveAttribute('max', '10');
-      expect(rangeSliderElement).toHaveAttribute('step', '0.5');
-    });
+      render(<RangeSlider formAction="post.php" min={4} max={10} step={0.5} />)
+      const rangeSliderElement = rangeSlider()
+      expect(rangeSliderElement).toHaveAttribute('formAction', 'post.php')
+      expect(rangeSliderElement).toHaveAttribute('min', '4')
+      expect(rangeSliderElement).toHaveAttribute('max', '10')
+      expect(rangeSliderElement).toHaveAttribute('step', '0.5')
+    })
 
     it('should be disabled when `disabled={true}`', () => {
-      render(<RangeSlider disabled />);
+      render(<RangeSlider disabled />)
 
-      expect(rangeSlider()).toBeDisabled();
-    });
+      expect(rangeSlider()).toBeDisabled()
+    })
 
     it('should be required when `required={true}`', () => {
-      render(<RangeSlider required={true} />);
-      expect(rangeSlider()).toHaveAttribute('required');
-    });
+      render(<RangeSlider required={true} />)
+      expect(rangeSlider()).toHaveAttribute('required')
+    })
 
     it('should allow ref as prop', () => {
-      const ref = createRef<HTMLInputElement>();
-      render(<RangeSlider ref={ref} name="range_slider_name" />);
-      expect(ref.current?.name).toBe('range_slider_name');
-    });
-  });
+      const ref = createRef<HTMLInputElement>()
+      render(<RangeSlider ref={ref} name="range_slider_name" />)
+      expect(ref.current?.name).toBe('range_slider_name')
+    })
+  })
 
   describe('Rendering', () => {
     it('should render with no props', () => {
-      render(<RangeSlider />);
-      expect(rangeSlider()).toBeInTheDocument();
-    });
-  });
+      render(<RangeSlider />)
+      expect(rangeSlider()).toBeInTheDocument()
+    })
+  })
 
   describe('Theme', () => {
     it('should use `base` classes', () => {
@@ -144,16 +144,16 @@ describe('Components / Button', () => {
             base: 'dummy-range-slider-base-classes',
           },
         },
-      };
+      }
 
       render(
         <PoluiProvider theme={{ theme }}>
           <RangeSlider />
         </PoluiProvider>,
-      );
+      )
 
-      expect(rangeSliderContainer()).toHaveClass('dummy-range-slider-base-classes');
-    });
+      expect(rangeSliderContainer()).toHaveClass('dummy-range-slider-base-classes')
+    })
 
     it('should use `base` classes of field', () => {
       const theme: CustomPoluiTheme = {
@@ -162,16 +162,16 @@ describe('Components / Button', () => {
             base: 'dummy-range-slider-field-base-classes',
           },
         },
-      };
+      }
 
       render(
         <PoluiProvider theme={{ theme }}>
           <RangeSlider />
         </PoluiProvider>,
-      );
+      )
 
-      expect(rangeSliderContainer().childNodes[0]).toHaveClass('dummy-range-slider-field-base-classes');
-    });
+      expect(rangeSliderContainer().childNodes[0]).toHaveClass('dummy-range-slider-field-base-classes')
+    })
 
     it('should use `base` classes of input', () => {
       const theme: CustomPoluiTheme = {
@@ -182,16 +182,16 @@ describe('Components / Button', () => {
             },
           },
         },
-      };
+      }
 
       render(
         <PoluiProvider theme={{ theme }}>
           <RangeSlider />
         </PoluiProvider>,
-      );
+      )
 
-      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-base-classes');
-    });
+      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-base-classes')
+    })
 
     it('should use `sizes` classes of input', () => {
       const theme: CustomPoluiTheme = {
@@ -204,17 +204,17 @@ describe('Components / Button', () => {
             },
           },
         },
-      };
+      }
 
       render(
         <PoluiProvider theme={{ theme }}>
-          <RangeSlider sizing="lg" />
+          <RangeSlider size="lg" />
         </PoluiProvider>,
-      );
+      )
 
-      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-sizes-lg-classes');
-    });
-  });
+      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-sizes-lg-classes')
+    })
+  })
 
   describe('Theme as a prop', () => {
     it('should use `base` classes', () => {
@@ -222,24 +222,24 @@ describe('Components / Button', () => {
         root: {
           base: 'dummy-range-slider-base-classes',
         },
-      };
+      }
 
-      render(<RangeSlider theme={theme} />);
+      render(<RangeSlider theme={theme} />)
 
-      expect(rangeSliderContainer()).toHaveClass('dummy-range-slider-base-classes');
-    });
+      expect(rangeSliderContainer()).toHaveClass('dummy-range-slider-base-classes')
+    })
 
     it('should use `base` classes of field', () => {
       const theme: CustomPoluiTheme['rangeSlider'] = {
         field: {
           base: 'dummy-range-slider-field-base-classes',
         },
-      };
+      }
 
-      render(<RangeSlider theme={theme} />);
+      render(<RangeSlider theme={theme} />)
 
-      expect(rangeSliderContainer().childNodes[0]).toHaveClass('dummy-range-slider-field-base-classes');
-    });
+      expect(rangeSliderContainer().childNodes[0]).toHaveClass('dummy-range-slider-field-base-classes')
+    })
 
     it('should use `base` classes of input', () => {
       const theme: CustomPoluiTheme['rangeSlider'] = {
@@ -248,12 +248,12 @@ describe('Components / Button', () => {
             base: 'dummy-range-slider-field-input-base-classes',
           },
         },
-      };
+      }
 
-      render(<RangeSlider theme={theme} />);
+      render(<RangeSlider theme={theme} />)
 
-      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-base-classes');
-    });
+      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-base-classes')
+    })
 
     it('should use `sizes` classes of input', () => {
       const theme: CustomPoluiTheme['rangeSlider'] = {
@@ -264,15 +264,15 @@ describe('Components / Button', () => {
             },
           },
         },
-      };
+      }
 
-      render(<RangeSlider sizing="lg" theme={theme} />);
+      render(<RangeSlider size="lg" theme={theme} />)
 
-      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-sizes-lg-classes');
-    });
-  });
-});
+      expect(rangeSlider()).toHaveClass('dummy-range-slider-field-input-sizes-lg-classes')
+    })
+  })
+})
 
-const rangeSliderContainer = () => screen.getByTestId('ui-range-slider');
-const rangeSlider = (role = 'slider') => screen.getByRole(role);
-const rangeSliders = (role = 'slider') => screen.getAllByRole(role);
+const rangeSliderContainer = () => screen.getByTestId('ui-range-slider')
+const rangeSlider = (role = 'slider') => screen.getByRole(role)
+const rangeSliders = (role = 'slider') => screen.getAllByRole(role)
