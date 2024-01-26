4da847a233af34e238b1f5f8f43ccfd8b6c8e03a
Pol Gubau <gubaupol@gmail.com>
2024-01-26 19:52:32 +0100

    Add string-width package to package.json and yarn.lock

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -3,6 +3,3 @@ import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
 afterEach(() => cleanup())
-
-// eslint-disable-next-line no-undef
-global.ResizeObserver = ResizeObserver
