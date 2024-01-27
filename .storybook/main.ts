508beb66eb91c4e18f18a11e0d089435653df474
Pol Gubau <gubaupol@gmail.com>
2024-01-27 11:24:39 +0100

    Delete unused assets and update Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,5 @@
 import type { Preview } from '@storybook/react'
-import './style.css';
-
+import './style.css'
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
