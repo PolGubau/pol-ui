adb4e8787d8cf2bc8268ab9057543ebf17121b93
Pol Gubau <gubaupol@gmail.com>
2024-02-09 22:18:54 +0100

    Refactor Avatar and AvatarGroup story decorators

--- src/components/Avatar/AvatarGroup.stories.tsx
+++ src/components/Avatar/AvatarGroup.stories.tsx
@@ -8,10 +8,8 @@ export default {
   tags: ['autodocs'],
   decorators: [
     Story => (
-      <div className="flex p-6 flex-col  min-h-[400px] justify-center items-center bg-secondary-50">
-        <div className="max-w-xl">
-          <Story />
-        </div>
+      <div className="flex p-6 flex-col justify-center items-center ">
+        <Story />
       </div>
     ),
   ],
