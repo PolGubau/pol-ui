b74b7bfd36f82239b7ca82418994c35722210e88
Pol Gubau <gubaupol@gmail.com>
2024-02-20 13:56:35 +0100

    Pol Gubau; MW-

--- src/components/ContainerScroll/ContainerScroll.tsx
+++ src/components/ContainerScroll/ContainerScroll.tsx
@@ -132,7 +132,7 @@ export const Card = ({
   return (
     <motion.div
       style={{
-        rotateX: rotate, // rotate in X-axis
+        rotateX: rotate,
         scale,
         boxShadow:
           '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
