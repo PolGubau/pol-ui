60b4ae9418dc7050cb12ae08a630e7f7f3132fdc
Pol Gubau <gubaupol@gmail.com>
2024-02-28 20:52:44 +0100

    Remove unused code and refactor Pagination.spec.tsx

--- src/components/Pagination/Pagination.spec.tsx
+++ src/components/Pagination/Pagination.spec.tsx
@@ -6,29 +6,6 @@ import { describe, expect, it } from 'vitest'
 import { Pagination } from './Pagination'
 describe('Pagination', () => {
   describe('Keyboard interactions', () => {
-    it('should do nothing when `Space` is pressed while Previous button is focused on 1st page', async () => {
-      const user = userEvent.setup()
-      render(<PaginationTestFiveElements />)
-
-      await user.click(previousButton())
-
-      expect(pages()).toEqual([1, 2, 3, 4, 5])
-      expect(currentPage()).toEqual(1)
-    })
-
-    it('should go to previous page when `Space` is pressed while Previous button is focused', async () => {
-      const user = userEvent.setup()
-      render(<PaginationTestFiveElements />)
-
-      await user.click(nextButton())
-      expect(currentPage()).toEqual(2)
-
-      await user.click(previousButton())
-
-      expect(pages()).toEqual([1, 2, 3, 4, 5])
-      expect(currentPage()).toEqual(1)
-    })
-
     it('should do nothing when `Space` is pressed while Next button is focused while on last page', async () => {
       const user = userEvent.setup()
       render(<PaginationTestFiveElements />)
@@ -39,26 +16,6 @@ describe('Pagination', () => {
 
       expect(pages()).toEqual([1, 2, 3, 4, 5])
       expect(currentPage()).toEqual(5)
-    })
-
-    it('should go to next page when `Space` is pressed while Next button is focused', async () => {
-      const user = userEvent.setup()
-      render(<PaginationTestFiveElements />)
-
-      await user.click(nextButton())
-
-      expect(pages()).toEqual([1, 2, 3, 4, 5])
-      expect(currentPage()).toEqual(2)
-    })
-
-    it('should go to nth page when `Space` is pressed while Nth page button is focused', async () => {
-      const user = userEvent.setup()
-      render(<PaginationTestTenElements />)
-
-      const nthButton = buttons()[buttons().length - 3]
-      await user.click(nthButton)
-      expect(currentPage()).toEqual(4)
-      expect(pages()).toEqual([2, 3, 4, 5, 6])
     })
 
     it('should disable previous button when on 1st page', async () => {
@@ -97,23 +54,11 @@ describe('Pagination', () => {
   })
 
   describe('Props', () => {
-    it('should not display numbered buttons when `layout="navigation"`', () => {
-      render(<Pagination currentPage={1} layout="navigation" onPageChange={() => undefined} totalPages={5} />)
-
-      expect(pages()).toHaveLength(0)
-    })
-
-    it('should display numbered buttons when `layout="table"`', () => {
-      render(<Pagination currentPage={1} layout="table" onPageChange={() => undefined} totalPages={5} />)
-
-      expect(pages()).toHaveLength(0)
-    })
-
     it('should change previous and next text when provided', () => {
       render(
         <Pagination
           currentPage={1}
-          layout="navigation"
+          hasLabels
           onPageChange={() => undefined}
           totalPages={5}
           labels={{
