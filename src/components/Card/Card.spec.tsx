52f27ac5e338e379d0a87f36c1492cd23d87b024
Pol Gubau <pol.gubau@meona.de>
2024-04-02 19:19:14 +0200

    Refactor Card component and remove unused code

--- src/components/Card/Card.spec.tsx
+++ src/components/Card/Card.spec.tsx
@@ -1,62 +1,14 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { PoluiProvider, type CustomPoluiTheme } from '../PoluiProvider'
 import { Card } from './Card'
 
 describe('Components / Card', () => {
-  describe('Functionality', () => {
-    it('should render an image when `imgSrc` is provided', () => {
-      render(<Card imgSrc="https://polgubau.com/_next/image?url=%2Fimages%2Fme.png&w=256&q=75" />)
-      expect(screen.queryAllByTestId('ui-card-image')).toHaveLength(1)
-      expect(screen.queryByTestId('ui-card-image')).toHaveAttribute(
-        'src',
-        'https://polgubau.com/_next/image?url=%2Fimages%2Fme.png&w=256&q=75',
-      )
-    })
-    it('should not render an `<img>` given an undefined `imgSrc`', () => {
-      render(<Card imgSrc={undefined} />)
-      expect(screen.queryAllByTestId('ui-card-image')).toHaveLength(0)
-    })
-
-    it('should render the image from the `renderImage` prop', () => {
-      render(<Card renderImage={() => <div data-testid="dummy-div" />} />)
-
-      expect(screen.queryAllByTestId('dummy-div')).toHaveLength(1)
-    })
-    it('should use the `renderImage` prop even if the user provides an `imgSrc`', () => {
-      render(
-        /* @ts-expect-error should be illegal to use `renderImage` and `imgSrc` at the same time */
-        <Card
-          renderImage={() => <div data-testid="dummy-div2" />}
-          imgSrc="https://polgubau.com/_next/image?url=%2Fimages%2Fme.png&w=256&q=75"
-        />,
-      )
-      expect(screen.queryAllByTestId('dummy-div2')).toHaveLength(1)
-      expect(screen.queryAllByTestId('ui-card-image')).toHaveLength(0)
-    })
-    it('should provide the theme and horizontal flag to the `renderImage` function', () => {
-      const spy = vi.fn(() => <div data-testid="dummy-div2" />)
-      render(<Card renderImage={spy} />)
-      expect(spy).toHaveBeenCalledWith(expect.any(Object), false)
-    })
-  })
   describe('A11y', () => {
     it('should allow `aria-label`', () => {
       render(<Card aria-label="My card" />)
 
       expect(card()).toHaveAccessibleName('My card')
-    })
-
-    it('should provide `<img />` with alternative text given `imgSrc="..."` and `imgAlt="..."`', () => {
-      render(
-        <Card
-          imgAlt="Meaningful alt text for an image that is not purely decorative"
-          imgSrc="https://polgubau.com/_next/image?url=%2Fimages%2Fme.png&w=256&q=75"
-        />,
-      )
-      const img = screen.getByAltText('Meaningful alt text for an image that is not purely decorative')
-
-      expect(img).toBeInTheDocument()
     })
   })
 
@@ -65,13 +17,6 @@ describe('Components / Card', () => {
       render(<Card href="#" />)
 
       expect(screen.getByRole('link')).toEqual(card())
-    })
-
-    it('should render an `<img>` given `imgSrc=".."`', () => {
-      render(<Card imgSrc="https://polgubau.com/_next/image?url=%2Fimages%2Fme.png&w=256&q=75" />)
-      const img = screen.getByRole('img')
-
-      expect(card()).toContainElement(img)
     })
   })
 
@@ -113,30 +58,6 @@ describe('Components / Card', () => {
       expect(children.parentElement).toHaveClass('text-cyan-900')
     })
 
-    it('should use `horizontal` classes', () => {
-      const theme: CustomPoluiTheme = {
-        card: {
-          root: {
-            horizontal: {
-              off: 'text-cyan-200',
-              on: 'text-cyan-300',
-            },
-          },
-        },
-      }
-      render(
-        <PoluiProvider theme={{ theme }}>
-          <Card />
-          <Card horizontal />
-        </PoluiProvider>,
-      )
-      const normalCard = cards()[0]
-      const horizontalCard = cards()[1]
-
-      expect(normalCard).toHaveClass('text-cyan-200')
-      expect(horizontalCard).toHaveClass('text-cyan-300')
-    })
-
     it('should use `href` classes', () => {
       const theme: CustomPoluiTheme = {
         card: {
@@ -157,5 +78,3 @@ describe('Components / Card', () => {
 })
 
 const card = () => screen.getByTestId('ui-card')
-
-const cards = () => screen.getAllByTestId('ui-card')
--- src/components/Sidebar/Sidebar.spec.tsx
+++ src/components/Sidebar/Sidebar.spec.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import type { FC } from 'react'
 import { HiChartPie, HiInbox, HiShoppingBag } from 'react-icons/hi'
 import { describe, expect, it } from 'vitest'
@@ -36,91 +35,15 @@ describe('Components / Sidebar', () => {
   })
 })
 
-describe('Keyboard interactions', () => {
-  it('should expand/collapse when `Space` is pressed on a `Sidebar.Collapse`', async () => {
-    const user = userEvent.setup()
-    render(<TestSidebar />)
-
-    const collapseButton = collapseButtons()[0]
-
-    await user.click(collapseButton)
-
-    const collapse = collapses()[0]
-
-    expect(collapse).toBeVisible()
-  })
-
-  it('should follow link when `Space` is pressed on `SidebarItem` with `href=".."`', () => {
-    render(<TestSidebar />)
-
-    const link = screen.getAllByRole('link')[1]
-
-    expect(link).toHaveAccessibleName('Dashboard')
-    expect(link).toHaveAttribute('href', '#')
-  })
-})
-
 describe('Props', () => {
   it("shouldn't display text content in `Sidebar.Logo` when `open={true}`", () => {
     render(<TestSidebar open />)
 
     expect(logo().lastElementChild).toHaveClass('hidden')
   })
-
-  it('should use the HTML element provided in `SidebarItem as=".."`', () => {
-    render(<TestSidebar />)
-
-    const asItem = screen.getByLabelText('My heading')
-
-    expect(asItem.tagName.toLocaleLowerCase()).toEqual('h3')
-  })
 })
 
 describe('Theme', () => {
-  describe('`SidebarItem`', () => {
-    it('should use custom classes', () => {
-      const theme: CustomPoluiTheme = {
-        sidebar: {
-          item: {
-            active: 'text-gray-100',
-            base: 'bg-gray-100',
-            collapsed: {
-              insideCollapse: 'text-gray-300',
-            },
-            content: {
-              base: 'bg-gray-200',
-            },
-            icon: {
-              base: 'text-gray-400',
-              active: 'bg-gray-300',
-            },
-          },
-        },
-      }
-
-      render(
-        <PoluiProvider theme={{ theme }}>
-          <TestSidebar open />
-        </PoluiProvider>,
-      )
-      const theItems = items()
-        .map(item => item.firstElementChild)
-        .map(item => item?.firstElementChild)
-        .filter(item => item?.tagName.toLocaleLowerCase() !== 'button') as HTMLElement[]
-      const activeItems = screen.getAllByTestId('active-item')
-      const activeIcons = activeItems.map(item => item.firstElementChild)
-      const inactiveIcons = [...collapseIcons().filter(icon => !activeIcons.includes(icon))]
-      const inactiveItems = [...theItems.filter(item => item !== null && !activeItems.includes(item))]
-
-      activeIcons.forEach(icon => expect(icon).toHaveClass('bg-gray-300'))
-      activeItems.forEach(item => expect(item).toHaveClass('text-gray-100'))
-      itemContents().forEach(content => expect(content).toHaveClass('bg-gray-200'))
-      inactiveIcons.forEach(icon => expect(icon).not.toHaveClass('bg-gray-300'))
-      inactiveItems.forEach(item => expect(item).not.toHaveClass('text-gray-100'))
-      icons().forEach(icon => expect(icon).toHaveClass('text-gray-400'))
-    })
-  })
-
   describe('`Sidebar.Logo`', () => {
     it('should use custom classes', () => {
       const theme: CustomPoluiTheme = {
@@ -165,17 +88,5 @@ const TestSidebar: FC<SidebarProps> = ({ ...props }) => (
     <SidebarItem as="h3">My heading</SidebarItem>
   </Sidebar>
 )
-
-const collapseButtons = () => screen.getAllByRole('button')
-
-const collapses = () => screen.getAllByRole('list').slice(1)
-
-const collapseIcons = () => screen.getAllByTestId('ui-sidebar-collapse-icon')
-
-const itemContents = () => screen.getAllByTestId('ui-sidebar-item-content')
-
-const icons = () => screen.getAllByTestId('ui-sidebar-item-icon')
-
-const items = () => screen.getAllByRole('listitem')
 
 const logo = () => screen.getByLabelText('PolUi')
