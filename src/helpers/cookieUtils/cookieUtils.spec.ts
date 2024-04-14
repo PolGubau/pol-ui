8d2163a5c057eac567dfb01dbd317d9369d81081
Pol Gubau Amores <gubaupol@gmail.com>
2024-04-14 20:36:45 +0200

    Pol Gubau; MW-

---
+++ src/helpers/cookieUtils/cookieUtils.spec.ts
@@ -0,0 +1,32 @@
+// cookieUtils.test.ts
+
+import { beforeEach, describe, expect, test } from 'vitest'
+import { setCookie, getCookie, deleteCookie } from './cookieUtils'
+
+describe('Cookie Utils', () => {
+  beforeEach(() => {
+    document.cookie = '' // Clear all cookies before each test
+  })
+
+  test('setCookie should set a cookie with the specified name and value', () => {
+    setCookie('testCookie', 'cookieValue')
+    expect(document.cookie).toContain('testCookie=cookieValue')
+  })
+
+  test('getCookie should return the value of an existing cookie', () => {
+    document.cookie = 'existingCookie=existingValue'
+    const value = getCookie('existingCookie')
+    expect(value).toBe('existingValue')
+  })
+
+  test('getCookie should return null for a non-existing cookie', () => {
+    const value = getCookie('nonExistingCookie')
+    expect(value).toBeNull()
+  })
+
+  test('deleteCookie should delete an existing cookie', () => {
+    document.cookie = 'toBeDeletedCookie=toBeDeletedValue'
+    deleteCookie('toBeDeletedCookie')
+    expect(document.cookie).not.toContain('toBeDeletedCookie')
+  })
+})
---
+++ src/helpers/cookieUtils/cookieUtils.ts
@@ -0,0 +1,37 @@
+// cookieUtils.ts
+/**
+ * Sets a cookie with the specified name, value, and optional expiration in days.
+ * @param {string} name - The name of the cookie.
+ * @param {string} value - The value to be stored in the cookie.
+ * @param {number} [days] - Optional number of days until the cookie expires.
+ */
+export const setCookie = (name: string, value: string, days?: number): void => {
+  const expirationDate = new Date()
+  expirationDate.setDate(expirationDate.getDate() + (days ?? 0))
+
+  const cookieValue = encodeURIComponent(value) + (days ? `; expires=${expirationDate.toUTCString()}` : '')
+  document.cookie = `${name}=${cookieValue}; path=/`
+}
+
+/**
+ * Retrieves the value of a cookie by its name.
+ * @param {string} name - The name of the cookie to retrieve.
+ * @returns {string|null} The value of the cookie if found, or null if not found.
+ */
+export const getCookie = (name: string): string | null => {
+  const cookies = document.cookie.split('; ')
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=')
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue)
+    }
+  }
+  return null
+}
+/**
+ * Deletes a cookie by setting its expiration date to the past.
+ * @param {string} name - The name of the cookie to delete.
+ */
+export const deleteCookie = (name: string): void => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`
+}
