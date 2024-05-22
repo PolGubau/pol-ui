95bdc6b030a3ddee9c1ec85702a821b954b385db
Pol Gubau Amores <gubaupol@gmail.com>
2024-05-22 18:25:34 +0200

    chore: Add AnimatedNumber component and update tsconfig.json

---
+++ src/components/AnimatedNumber/AnimatedNumber.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta, StoryFn } from '@storybook/react'
+import { useState } from 'react'
+import { TbReload } from 'react-icons/tb'
+import { IconButton } from '../IconButton'
+import type { AnimatedNumberProps } from './AnimatedNumber'
+import AnimatedNumber from './AnimatedNumber'
+
+export default {
+  title: 'Components/AnimatedNumber',
+  component: AnimatedNumber,
+
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div className="flex p-6 flex-col justify-center items-center">
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta
+
+const Template: StoryFn<AnimatedNumberProps> = props => {
+  const [key, setKey] = useState(0)
+  function sum() {
+    setKey(key + 1)
+  }
+  return (
+    <div className="grid place-items-center min-h-[200px] relative w-full">
+      <IconButton onClick={sum} className="absolute top-2 right-2">
+        <TbReload />
+      </IconButton>
+      <AnimatedNumber {...props} key={key} />
+    </div>
+  )
+}
+
+export const Default = Template.bind({})
+
+export const CustomProps = Template.bind({})
+CustomProps.parameters = {
+  docs: {
+    description: {
+      story: 'This story shows the `className` prop set to `text-4xl text-green-400 font-bold`.',
+    },
+  },
+}
+CustomProps.args = {
+  className: 'text-4xl text-green-400 font-bold',
+}
+export const CustomDuration = Template.bind({})
+CustomDuration.parameters = {
+  docs: {
+    description: {
+      story: 'This story shows the `duration` prop set to 15.',
+    },
+  },
+}
+CustomDuration.args = {
+  duration: 15,
+}
+export const CustomUntil = Template.bind({})
+CustomUntil.parameters = {
+  docs: {
+    description: {
+      story: 'This story shows the `until` prop set to 777.',
+    },
+  },
+}
+CustomUntil.args = {
+  until: 777,
+}
+export const CustomSince = Template.bind({})
+CustomSince.parameters = {
+  docs: {
+    description: {
+      story: 'This story shows the `from` prop set to 50.',
+    },
+  },
+}
+
+CustomSince.args = {
+  from: 50,
+}
---
+++ src/components/AnimatedNumber/AnimatedNumber.tsx
@@ -0,0 +1,33 @@
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion'
+import { useEffect } from 'react'
+import { DynamicHeading } from '../DynamicHeading'
+import type { HeadingLevel } from '../../types'
+
+export interface AnimatedNumberProps extends React.HTMLAttributes<HTMLElement> {
+  as?: HeadingLevel
+  duration?: number
+  from?: number
+  until?: number
+}
+
+export default function AnimatedNumber({
+  as = 'h6',
+  from = 0,
+  until = 100,
+  duration = 2,
+  ...rest
+}: Readonly<AnimatedNumberProps>) {
+  const count = useMotionValue(from)
+  const rounded = useTransform(count, Math.round)
+
+  useEffect(() => {
+    const animation = animate(count, until, { duration })
+    return animation.stop
+  }, [count, duration, until])
+
+  return (
+    <DynamicHeading as={as} {...rest}>
+      <motion.span>{rounded}</motion.span>
+    </DynamicHeading>
+  )
+}
