874f8b144577d38a19b9bc89302da8d9e8090309
Pol Gubau Amores <gubaupol@gmail.com>
2024-06-03 12:32:59 +0200

    refactor: Update Popover component to use render prop for content and trigger

--- src/components/Popover/Popover.spec.tsx
+++ src/components/Popover/Popover.spec.tsx
@@ -3,25 +3,18 @@ import { render } from '@testing-library/react'
 import { Popover } from './Popover'
 import { test, describe, expect } from 'vitest'
 import userEvent from '@testing-library/user-event'
+const Example = <Popover label="Click me">Popover Content</Popover>
 
 describe('Popover component', () => {
   test('renders popover content when trigger is clicked', async () => {
-    const { getByText } = render(
-      <Popover content="Popover Content">
-        <span>Click me</span>
-      </Popover>,
-    )
+    const { getByText } = render(Example)
     await userEvent.click(getByText('Click me'))
 
     expect(getByText('Popover Content')).toBeInTheDocument()
   })
 
   test('hides popover content when trigger is clicked again', async () => {
-    const { getByText, queryByText } = render(
-      <Popover content="Popover Content">
-        <span>Click me</span>
-      </Popover>,
-    )
+    const { getByText, queryByText } = render(Example)
     await userEvent.click(getByText('Click me'))
     await userEvent.click(getByText('Click me'))
 
@@ -29,15 +22,9 @@ describe('Popover component', () => {
   })
 
   test('renders popover content when trigger is clicked and modal is true', async () => {
-    const { getByText } = render(
-      <Popover content="Popover Content" modal>
-        <span>Click me</span>
-      </Popover>,
-    )
+    const { getByText } = render(Example)
     await userEvent.click(getByText('Click me'))
 
     expect(getByText('Popover Content')).toBeInTheDocument()
   })
-
-  // Add more tests as needed...
 })
