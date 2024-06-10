81223281526b2df2d66263cb3d37f893b1cf2111
Pol Gubau Amores <gubaupol@gmail.com>
2024-06-10 11:13:55 +0200

    chore: Update DialogTitle component styles

--- packages/ui/src/components/Dialog/extras/title.tsx
+++ packages/ui/src/components/Dialog/extras/title.tsx
@@ -4,7 +4,11 @@ import { Title } from '@radix-ui/react-dialog'
 
 export const DialogTitle = forwardRef<React.ElementRef<typeof Title>, React.ComponentPropsWithoutRef<typeof Title>>(
   ({ className, ...props }, ref) => (
-    <Title ref={ref} className={cn('text-lg font-semibold leading-none tracking-tight', className)} {...props} />
+    <Title
+      ref={ref}
+      className={cn('text-lg font-semibold leading-none tracking-tight text-black dark:text-white', className)}
+      {...props}
+    />
   ),
 )
 DialogTitle.displayName = Title.displayName
