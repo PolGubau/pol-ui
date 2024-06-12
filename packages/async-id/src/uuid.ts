18d3eb185ffdec60d70071369c21188384c27711
Pol Gubau Amores <gubaupol@gmail.com>
2024-06-12 23:34:09 +0200

    refactor: Simplify GitHub Actions workflow condition for async-id package

--- packages/async-id/src/uuid.ts
+++ packages/async-id/src/uuid.ts
@@ -15,6 +15,7 @@ export const generateUidFunction =
         reject(new Error('You must supply a length integer greater than zero'));
         return;
       }
+
       randomBytes(len, (err, buf) => {
         if (err) {
           return reject(err);
