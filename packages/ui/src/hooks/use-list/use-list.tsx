a2da0ecf9c37b50ba8e1eb3e7c4f76d712a11051
Pol Gubau Amores <gubaupol@gmail.com>
2024-08-24 13:24:52 +0200

    feat: Add @radix-ui/react-accordion dependency for UI package
    This commit adds the @radix-ui/react-accordion dependency to the UI package. This new dependency will enable the implementation of accordion components in the UI package, providing a collapsible and expandable UI element. This feature enhances the user experience by allowing users to hide or reveal content as needed.
    
    Closes #456

---
+++ packages/ui/src/hooks/use-list/use-list.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react"
+
+export type CustomList<T> = {
+  set: (l: T[]) => void
+  push: (element: T) => void
+  removeAt: (index: number) => void
+  insertAt: (index: number, element: T) => void
+  updateAt: (index: number, element: T) => void
+  clear: () => void
+}
+
+export function useList<T>(defaultList: T[] = []): [T[], CustomList<T>] {
+  const [list, setList] = useState(defaultList)
+
+  const set = useCallback((l: T[]) => {
+    setList(l)
+  }, [])
+
+  const push = useCallback((element: T) => {
+    setList((l) => [...l, element])
+  }, [])
+
+  const removeAt = useCallback((index: number) => {
+    setList((l) => [...l.slice(0, index), ...l.slice(index + 1)])
+  }, [])
+
+  const insertAt = useCallback((index: number, element: T) => {
+    setList((l) => [...l.slice(0, index), element, ...l.slice(index)])
+  }, [])
+
+  const updateAt = useCallback((index: number, element: T) => {
+    setList((l) => l.map((e, i) => (i === index ? element : e)))
+  }, [])
+
+  const clear = useCallback(() => setList([]), [])
+
+  return [list, { set, push, removeAt, insertAt, updateAt, clear }]
+}
