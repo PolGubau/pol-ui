4a1c5d8e8dd63662ea7d66e3b460045b30bd0e92
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-07 20:03:05 +0200

    refactor: Add shuffle and clamp helper functions
    - Added the shuffle helper function to randomize the order of elements in an array using the Fisher-Yates algorithm.
    - Added the clamp helper function to clamp a number within specified lower and upper bounds.
    
    Refactor the code to remove unused files and update dependencies.
    
    Related to #123

--- packages/ui/src/helpers/omit/omit.ts
+++ packages/ui/src/helpers/omit/omit.ts
@@ -1,15 +1,29 @@
-export const omit =
-  <T extends object, K extends string>(keys: readonly K[]) =>
-  (obj: T): Omit<T, K> => {
-    const result = {} as Omit<T, K>
+/**
+ * Creates a new object with specified keys omitted.
+ *
+ * This function takes an object and an array of keys, and returns a new object that
+ * excludes the properties corresponding to the specified keys.
+ *
+ * @template T - The type of object.
+ * @template K - The type of keys in object.
+ * @param {T} obj - The object to omit keys from.
+ * @param {K[]} keys - An array of keys to be omitted from the object.
+ * @returns {Omit<T, K>} A new object with the specified keys omitted.
+ *
+ * @example
+ * const obj = { a: 1, b: 2, c: 3 };
+ * const result = omit(obj, ['b', 'c']);
+ * // result will be { a: 1 }
+ */
+export function omit<T extends Record<string, any>, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Omit<T, K> {
+  const result = { ...obj }
 
-    for (const key in obj) {
-      // @ts-expect-error - bypass the type check
-      if (keys.includes(key)) {
-        continue
-      } // @ts-expect-error - bypass the type check
-      result[key] = obj[key]
-    }
-
-    return result
+  for (const key of keys) {
+    delete result[key]
   }
+
+  return result as Omit<T, K>
+}
--- packages/ui/src/helpers/regex/regex.ts
+++ packages/ui/src/helpers/regex/regex.ts
@@ -1,16 +1,18 @@
 export const cuidRegex = /^c[^\s-]{8,}$/i
 export const cuid2Regex = /^[0-9a-z]+$/
 export const ulidRegex = /^[0-9A-HJKMNP-TV-Z]{26}$/
-export const uuidRegex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i
+export const uuidRegex =
+  /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i
 export const nanoidRegex = /^[a-z0-9_-]{21}$/i
 export const durationRegex =
   /^[-+]?P(?!$)(?:(?:[-+]?\d+Y)|(?:[-+]?\d+[.,]\d+Y$))?(?:(?:[-+]?\d+M)|(?:[-+]?\d+[.,]\d+M$))?(?:(?:[-+]?\d+W)|(?:[-+]?\d+[.,]\d+W$))?(?:(?:[-+]?\d+D)|(?:[-+]?\d+[.,]\d+D$))?(?:T(?=[\d+-])(?:(?:[-+]?\d+H)|(?:[-+]?\d+[.,]\d+H$))?(?:(?:[-+]?\d+M)|(?:[-+]?\d+[.,]\d+M$))?(?:[-+]?\d+(?:[.,]\d+)?S)?)??$/
- 
-export const emailRegex = /^(?!\.)(?!.*\.\.)([A-Z0-9_'+\-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
+
+export const emailRegex =
+  /^(?!\.)(?!.*\.\.)([A-Z0-9_'+\-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
 
 // from https://thekevinscott.com/emojis-in-javascript/#writing-a-regular-expression
 export const _emojiRegex = `^(\\p{Extended_Pictographic}|\\p{Emoji_Component})+$`
-export const emojiRegex = new RegExp(_emojiRegex, 'u')
+export const emojiRegex = new RegExp(_emojiRegex, "u")
 
 // faster, simpler, safer
 export const ipv4Regex =
@@ -20,7 +22,8 @@ export const ipv6Regex =
   /^(([a-f0-9]{1,4}:){7}|::([a-f0-9]{1,4}:){0,6}|([a-f0-9]{1,4}:){1}:([a-f0-9]{1,4}:){0,5}|([a-f0-9]{1,4}:){2}:([a-f0-9]{1,4}:){0,4}|([a-f0-9]{1,4}:){3}:([a-f0-9]{1,4}:){0,3}|([a-f0-9]{1,4}:){4}:([a-f0-9]{1,4}:){0,2}|([a-f0-9]{1,4}:){5}:([a-f0-9]{1,4}:){0,1})([a-f0-9]{1,4}|(((25[0-5])|(2[0-4][0-9])|(1[0-9]{2})|([0-9]{1,2}))\.){3}((25[0-5])|(2[0-4][0-9])|(1[0-9]{2})|([0-9]{1,2})))$/
 
 // https://stackoverflow.com/questions/7860392/determine-if-string-is-in-base64-using-javascript
-export const base64Regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/
+export const base64Regex =
+  /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/
 
 // simple
 // export const dateRegexSource = `\\d{4}-\\d{2}-\\d{2}`;
