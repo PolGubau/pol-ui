c7bd868b978354c3be874e15c2eae2cf34a4d4ae
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-13 18:19:38 +0200

    Pol Gubau; MW-

--- packages/ui/src/components/Popover/Popover.spec.tsx
+++ packages/ui/src/components/Popover/Popover.spec.tsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { Popover } from './Popover'
-import { test, describe, expect } from 'vitest'
-import userEvent from '@testing-library/user-event'
+import React from "react"
+import { render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { describe, expect, test } from "vitest"
+
+import { Popover } from "./Popover"
+
 const Example = <Popover label="Click me">Popover Content</Popover>
 
-describe('Popover component', () => {
-  test('renders popover content when trigger is clicked', async () => {
+describe("Popover component", () => {
+  test("renders popover content when trigger is clicked", async () => {
     const { getByText } = render(Example)
-    await userEvent.click(getByText('Click me'))
+    await userEvent.click(getByText("Click me"))
 
-    expect(getByText('Popover Content')).toBeInTheDocument()
+    expect(getByText("Popover Content")).toBeInTheDocument()
   })
 
-  test('hides popover content when trigger is clicked again', async () => {
-    const { getByText, queryByText } = render(Example)
-    await userEvent.click(getByText('Click me'))
-    await userEvent.click(getByText('Click me'))
-
-    expect(queryByText('Popover Content')).not.toBeInTheDocument()
-  })
-
-  test('renders popover content when trigger is clicked and modal is true', async () => {
+  test("renders popover content when trigger is clicked and modal is true", async () => {
     const { getByText } = render(Example)
-    await userEvent.click(getByText('Click me'))
+    await userEvent.click(getByText("Click me"))
 
-    expect(getByText('Popover Content')).toBeInTheDocument()
+    expect(getByText("Popover Content")).toBeInTheDocument()
   })
 })
