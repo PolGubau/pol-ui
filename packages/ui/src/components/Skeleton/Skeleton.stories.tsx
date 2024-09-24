b3eed49438101fcdd02f710f1c9d56b1ef0d7652
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-24 15:37:23 +0200

    refactor: Update Skeleton component to include new styles and attributes

--- packages/ui/src/components/Skeleton/Skeleton.stories.tsx
+++ packages/ui/src/components/Skeleton/Skeleton.stories.tsx
@@ -1,27 +1,21 @@
-import type { Meta, StoryFn } from '@storybook/react'
-import { Skeleton, SkeletonProps } from './Skeleton'
+import type { Meta } from "@storybook/react"
+
+import { Skeleton } from "./Skeleton"
 
 export default {
-  title: 'Components/Skeleton',
+  title: "Components/Skeleton",
   component: Skeleton,
-  tags: ['autodocs'],
-  decorators: [
-    (Story): JSX.Element => (
-      <div className="flex flex-col gap-10">
-        <div className="flex flex-col bg-secondary-50 min-h-10 p-4 gap-4">
-          In Light theme
-          <Story />
-        </div>
-        <div className="flex flex-col dark bg-secondary-900 min-h-10 p-4 gap-4 text-white">
-          In Dark theme
-          <Story />
-        </div>
-      </div>
-    ),
-  ],
+  tags: ["autodocs"],
 } as Meta
 
-const Template: StoryFn<SkeletonProps> = args => <Skeleton {...args} />
-
-export const Default = Template.bind({})
-Default.args = {}
+export const Default = () => {
+  return (
+    <div className="flex gap-4">
+      <div className="h-[30px] flex gap-2">
+        <Skeleton className="h-full aspect-square rounded-full"></Skeleton>
+        <Skeleton className="h-full w-[80px]"></Skeleton>
+        <Skeleton className="h-full w-[160px]"></Skeleton>
+      </div>
+    </div>
+  )
+}
