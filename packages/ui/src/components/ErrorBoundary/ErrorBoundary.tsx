8e7ccdcff17dad9648a178c131bd3b0545c13508
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-24 18:22:28 +0200

    refactor: Update component imports and add ErrorBoundary component

---
+++ packages/ui/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,67 @@
+import React, { ErrorInfo, PropsWithChildren, ReactNode } from "react"
+import { TbReload } from "react-icons/tb"
+
+import { IconButton } from "../IconButton"
+
+interface ErrorBoundaryProps
+  extends PropsWithChildren<{
+    fallbackUI: ReactNode
+    onError?: (error: Error, errorInfo: ErrorInfo) => void
+    retry?: () => void
+    reloadText?: string
+  }> {}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    const { onError } = this.props
+    if (onError) {
+      onError(error, errorInfo)
+    }
+  }
+
+  handleRetry = (): void => {
+    const { retry } = this.props
+    if (retry) {
+      retry()
+      this.setState({ hasError: false })
+    }
+  }
+
+  render(): React.ReactNode {
+    const { fallbackUI, children, reloadText = "Reload" } = this.props
+    const { hasError } = this.state
+
+    if (hasError) {
+      return (
+        <div className="flex gap-2 items-center">
+          {fallbackUI}
+          {this.props.retry && (
+            <IconButton label={reloadText} onClick={this.handleRetry}>
+              <TbReload />
+            </IconButton>
+          )}
+        </div>
+      )
+    }
+
+    return children
+  }
+}
+
+export default ErrorBoundary
