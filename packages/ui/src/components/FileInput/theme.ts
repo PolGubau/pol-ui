32e99e904d50c84f03db668d11659ecedc9fcda6
Pol Gubau Amores <gubaupol@gmail.com>
2024-11-22 08:52:42 +0100

    refactor: Standardize string quotes in FileInput theme.ts for consistency

--- packages/ui/src/components/FileInput/theme.ts
+++ packages/ui/src/components/FileInput/theme.ts
@@ -1,4 +1,4 @@
-import type { ColorsType, MainSizesType } from '../../types'
+import type { ColorsType, MainSizesType } from "../../types"
 
 export interface FileInputTheme {
   root: {
@@ -16,26 +16,27 @@ export interface FileInputTheme {
 
 export const fileInputTheme: FileInputTheme = {
   root: {
-    base: 'flex',
+    base: "flex",
   },
   field: {
-    base: 'relative w-full',
+    base: "relative w-full",
     input: {
-      base: 'rounded-lg  file:rounded-lg overflow-hidden flex w-full disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer file:cursor-pointer file:z-50 file:border-none file:px-4 file:py-2 focus:ring-2 focus:ring-offset-2 focus:ring-offset-secondary-50 outline-none focus:outline-none transition-all',
+      base: "rounded-lg  file:rounded-lg overflow-hidden flex w-full disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer file:cursor-pointer file:z-50 file:border-none file:px-4 file:py-2 focus:ring-2 focus:ring-offset-2 focus:ring-offset-secondary-50 outline-none focus:outline-none transition-all",
       size: {
-        xs: 'sm:text-xs',
-        sm: 'sm:text-sm',
-        md: 'sm:text-md',
-        lg: 'sm:text-lg',
-        xl: 'sm:text-xl',
+        xs: "sm:text-xs",
+        sm: "sm:text-sm",
+        md: "sm:text-md",
+        lg: "sm:text-lg",
+        xl: "sm:text-xl",
       },
       colors: {
-        info: 'file:bg-info/60 file:text-info-900 file:placeholder-info-800 focus:ring-info',
-        error: 'file:bg-error/60 file:text-error-900 file:placeholder-error-800  focus:ring-error',
-        success: 'file:bg-success/60 file:text-success-900 file:placeholder-success-800  focus:ring-success',
-        warning: 'file:bg-warning/60 file:text-warning-900 file:placeholder-warning-800  focus:ring-warning',
-        primary: 'file:bg-primary/60 file:text-primary-900 file:placeholder-primary-800  focus:ring-primary',
-        secondary: 'file:bg-secondary/60 file:text-secondary-900 file:placeholder-secondary-800  focus:ring-secondary',
+        info: "file:bg-info/60 file:text-info-900 focus:ring-info",
+        error: "file:bg-error/60 file:text-error-900 focus:ring-error",
+        success: "file:bg-success/60 file:text-success-900 focus:ring-success",
+        warning: "file:bg-warning/60 file:text-warning-900 focus:ring-warning",
+        primary: "file:bg-primary/60 file:text-primary-900 focus:ring-primary",
+        secondary:
+          "file:bg-secondary/60 file:text-secondary-900 focus:ring-secondary",
       },
     },
   },
