46e033e7bb542fe438e2c339939d3f01d0e9e6bf
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-01 15:18:56 +0100

    chore: remove CHANGELOG.md file and clean up vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    // ...
-  },
+  test: {},
   plugins: [tsconfigPaths()],
 });
