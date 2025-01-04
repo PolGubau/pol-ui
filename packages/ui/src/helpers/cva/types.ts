15455a35ceef772551a080d6de12dc6cd985af64
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-04 11:31:08 +0100

    refactor(ui): replace clsx import with custom implementation and update type definitions

--- packages/ui/src/helpers/cva/types.ts
+++ packages/ui/src/helpers/cva/types.ts
@@ -1,18 +1,18 @@
-import type * as clsx from "clsx";
+import type { ClassValue } from "../cn/clsx";
+
 export type ClassPropKey = "class" | "className";
-export type ClassValue = clsx.ClassValue;
 export type ClassProp =
-  | {
-      class: ClassValue;
-      className?: never;
-    }
-  | {
-      class?: never;
-      className: ClassValue;
-    }
-  | {
-      class?: never;
-      className?: never;
-    };
+	| {
+			class: ClassValue;
+			className?: never;
+	  }
+	| {
+			class?: never;
+			className: ClassValue;
+	  }
+	| {
+			class?: never;
+			className?: never;
+	  };
 export type OmitUndefined<T> = T extends undefined ? never : T;
 export type StringToBoolean<T> = T extends "true" | "false" ? boolean : T;
