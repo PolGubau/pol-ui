61c18b5b6171b93fde5e28e05631980cadd36f35
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-25 16:35:04 +0100

    refactor(ui): ♻️ reorganize Storybook configuration for improved clarity

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,28 +2,28 @@ import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	staticDirs: ["../public"],
+	addons: [
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-interactions"),
+		getAbsolutePath("@storybook/addon-a11y"),
+		getAbsolutePath("@storybook/addon-mdx-gfm"),
+		getAbsolutePath("@storybook/addon-mdx-gfm"),
+	],
+	docs: {
+		defaultName: "Docs",
+		autodocs: true,
+	},
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
 
-  addons: [
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-  ],
-  docs: {
-    defaultName: "Docs",
-    autodocs: true,
-  },
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
-  },
-
-  typescript: {
-    check: false, // `false` is default value, but `checked` field is required in types.
-    reactDocgen: "react-docgen-typescript",
-  },
+	typescript: {
+		check: false, // `false` is default value, but `checked` field is required in types.
+		reactDocgen: "react-docgen-typescript",
+	},
 };
 
 export default config;
@@ -32,5 +32,5 @@ export default config;
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")));
 }
