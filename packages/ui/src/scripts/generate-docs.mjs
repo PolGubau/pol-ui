c2efb816dadb0e81ff03bf0e18cb86388b1c3167
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-25 14:03:00 +0100

    refactor: ♻️ improve documentation generation logic

--- packages/ui/src/scripts/generate-docs.mjs
+++ packages/ui/src/scripts/generate-docs.mjs
@@ -8,43 +8,37 @@ import { parse } from "react-docgen-typescript";
 const dirname = resolve();
 const componentsPath = join(dirname, "src/components");
 const options = {
-	savePropValueAsString: true,
+  savePropValueAsString: true,
 };
 // Función para extraer la información de los componentes
 const generateDocs = () => {
-	const components = sync(`${componentsPath}/**/*.tsx`);
-	const filteredComponents = components.filter(
-		(componentPath) =>
-			!(
-				componentPath.includes("test") ||
-				componentPath.includes("stories") ||
-				componentPath.includes("spec")
-			),
-	);
+  const components = sync(`${componentsPath}/**/*.tsx`);
+  const filteredComponents = components.filter(
+    (componentPath) =>
+      !(componentPath.includes("test") || componentPath.includes("stories") || componentPath.includes("spec")),
+  );
 
-	const apiData = {};
+  const apiData = {};
 
-	for (const componentPath of filteredComponents) {
-		const componentName = basename(componentPath, ".tsx");
-		const componentCode = readFileSync(componentPath, "utf-8");
+  for (const componentPath of filteredComponents) {
+    const componentName = basename(componentPath, ".tsx");
+    const componentCode = readFileSync(componentPath, "utf-8");
 
-		try {
-			// Usamos react-docgen para extraer la info
-			const doc = parse(componentCode, options);
-			console.info("▶️ Parsing component", componentName);
-			apiData[componentName] = doc;
-		} catch (error) {
-			console.error(
-				`Error al procesar el componente ${componentName}: ${error.message}`,
-			);
-		}
-	}
+    try {
+      // Usamos react-docgen para extraer la info
+      const doc = parse(componentCode, options);
+      console.info("▶️ Parsing component", componentName);
+      apiData[componentName] = doc;
+    } catch (error) {
+      console.error(`Error al procesar el componente ${componentName}: ${error.message}`);
+    }
+  }
 
-	// Escribir el JSON en el directorio public
-	const apiJsonPath = join(dirname, "public", "api.json");
-	writeFileSync(apiJsonPath, JSON.stringify(apiData, null, 2));
+  // Escribir el JSON en el directorio public
+  const apiJsonPath = join(dirname, "public", "api.json");
+  writeFileSync(apiJsonPath, JSON.stringify(apiData, null, 2));
 
-	console.info("🟢 Documentación generada correctamente en api.json");
+  console.info("🟢 Documentación generada correctamente en api.json");
 };
 
 // Ejecutar la generación de documentación
