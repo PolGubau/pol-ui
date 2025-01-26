558b14ff5e546bcfe50dc19b690b1abbb0476267
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-26 11:41:55 +0100

    refactor(ui): ♻️ improve code formatting and readability in Chip component

--- packages/ui/src/components/Chip/Chip.tsx
+++ packages/ui/src/components/Chip/Chip.tsx
@@ -7,12 +7,7 @@ import { cn, colorToTailwind, mergeDeep } from "../../helpers";
 import type { MinimalEvent } from "../../hooks/use-ripple/use-ripple";
 import { useRipple } from "../../hooks/use-ripple/use-ripple";
 import { getTheme } from "../../theme-store";
-import {
-	type Colors,
-	ColorsEnum,
-	type DeepPartial,
-	type RoundedSizes,
-} from "../../types";
+import { type Colors, ColorsEnum, type DeepPartial, type RoundedSizes } from "../../types";
 import { IconButton } from "../IconButton/IconButton";
 import type { ChipTheme } from "./theme";
 
@@ -20,25 +15,25 @@ const BASE_COMPONENT = "li";
 type BaseComponentType = typeof BASE_COMPONENT;
 
 export interface ChipAction extends React.HTMLAttributes<HTMLButtonElement> {
-	element?: React.ReactNode;
-	icon?: React.ReactNode;
-	label: string;
+  element?: React.ReactNode;
+  icon?: React.ReactNode;
+  label: string;
 }
 
 export interface ChipProps<T extends ElementType = BaseComponentType> {
-	as?: T;
-	children?: React.ReactNode;
-	label?: React.ReactNode | string;
-	onClick?: (e: React.MouseEvent) => void;
-	color?: Colors;
-	disabled?: boolean;
-	rounded?: RoundedSizes;
-	actions?: ChipAction[];
-	theme?: DeepPartial<ChipTheme>;
-	className?: string;
-	elementClassName?: string;
-	textClassName?: string;
-	onDelete?: (e: React.MouseEvent) => void;
+  as?: T;
+  children?: React.ReactNode;
+  label?: React.ReactNode | string;
+  onClick?: (e: React.MouseEvent) => void;
+  color?: Colors;
+  disabled?: boolean;
+  rounded?: RoundedSizes;
+  actions?: ChipAction[];
+  theme?: DeepPartial<ChipTheme>;
+  className?: string;
+  elementClassName?: string;
+  textClassName?: string;
+  onDelete?: (e: React.MouseEvent) => void;
 }
 /**
  *  Chip component
@@ -74,76 +69,69 @@ export interface ChipProps<T extends ElementType = BaseComponentType> {
  */
 
 export const Chip = <T extends ElementType = BaseComponentType>({
-	as: BaseComponent,
-	children,
-	label = undefined,
-	onClick = undefined,
-	actions = [],
-	disabled = false,
-	color = ColorsEnum.secondary,
-	theme: customTheme = {},
-	className,
-	elementClassName,
-	textClassName,
-	onDelete,
+  as: BaseComponent,
+  children,
+  label = undefined,
+  onClick = undefined,
+  actions = [],
+  disabled = false,
+  color = ColorsEnum.secondary,
+  theme: customTheme = {},
+  className,
+  elementClassName,
+  textClassName,
+  onDelete,
 }: ChipProps<T>) => {
-	const Component = onClick ? "button" : (BaseComponent ?? BASE_COMPONENT);
-	const theme = mergeDeep(getTheme().chip, customTheme);
+  const Component = onClick ? "button" : (BaseComponent ?? BASE_COMPONENT);
+  const theme = mergeDeep(getTheme().chip, customTheme);
 
-	const [ripple, event] = useRipple({
-		disabled: disabled,
-		opacity: 0.5,
-		className: colorToTailwind(color),
-	});
+  const [ripple, event] = useRipple({
+    disabled: disabled,
+    opacity: 0.5,
+    className: colorToTailwind(color),
+  });
 
-	return (
-		<Component
-			className={cn(
-				theme.base,
-				actions.length === 0 ? theme.withoutAction : theme.withAction,
-				theme.color[color],
-				disabled && theme.disabled,
-				!disabled && onClick && theme.clickable,
-				className,
-			)}
-			onPointerDown={(e: MinimalEvent) => onClick && event(e)}
-			ref={onClick && ripple}
-			onClick={onClick}
-			data-testid="chip"
-		>
-			<div className={cn(theme.text, textClassName)}>{label ?? children}</div>
+  return (
+    <Component
+      className={cn(
+        theme.base,
+        actions.length === 0 ? theme.withoutAction : theme.withAction,
+        theme.color[color],
+        disabled && theme.disabled,
+        !disabled && onClick && theme.clickable,
+        className,
+      )}
+      onPointerDown={(e: MinimalEvent) => onClick && event(e)}
+      ref={onClick && ripple}
+      onClick={onClick}
+      data-testid="chip"
+    >
+      <div className={cn(theme.text, textClassName)}>{label ?? children}</div>
 
-			{/* The actions of the chip will be displayed as icons or elements */}
-			{onDelete && (
-				<IconButton
-					size="xs"
-					onClick={onDelete}
-					color={"error"}
-					disabled={disabled}
-				>
-					<TbX />
-				</IconButton>
-			)}
+      {/* The actions of the chip will be displayed as icons or elements */}
+      {onDelete && (
+        <IconButton size="xs" onClick={onDelete} color={"error"} disabled={disabled}>
+          <TbX />
+        </IconButton>
+      )}
 
-			{actions.map(({ onClick, element, icon, label }) => (
-				<div key={label}>
-					{icon && (
-						<IconButton
-							size="xs"
-							onClick={(e) => {
-								e.stopPropagation();
-								onClick?.(e);
-							}}
-							disabled={disabled}
-						>
-							{icon}
-						</IconButton>
-					)}
-					{element && (
-						<div className={cn(theme.element, elementClassName)}>{element}</div>
-					)}
-				</div>
-			))}
-		</Component>
-	);
+      {actions.map(({ onClick, element, icon, label }) => (
+        <div key={label}>
+          {icon && (
+            <IconButton
+              size="xs"
+              onClick={(e) => {
+                e.stopPropagation();
+                onClick?.(e);
+              }}
+              disabled={disabled}
+            >
+              {icon}
+            </IconButton>
+          )}
+          {element && <div className={cn(theme.element, elementClassName)}>{element}</div>}
+        </div>
+      ))}
+    </Component>
+  );
 };
