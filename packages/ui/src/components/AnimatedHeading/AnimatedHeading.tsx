8935c2ec9e72806615deee6d8fe2eba220f8c60e
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-01 14:54:55 +0100

    refactor(ui): ♻️ optimize component structure for better performance

--- packages/ui/src/components/AnimatedHeading/AnimatedHeading.tsx
+++ packages/ui/src/components/AnimatedHeading/AnimatedHeading.tsx
@@ -13,20 +13,17 @@ import type { DynamicHeadingProps } from "../DynamicHeading";
 import { DynamicHeading } from "../DynamicHeading";
 import type { AnimateHeadingTheme } from "./theme";
 import {
-	AnimatedHeadingAnimationsEnum,
-	type AnimatedHeadingsAnimation,
-	animatedHeadinganimations,
-	lettersAnimations,
-	wholeSenteceAnimations,
-	wordsAnimations,
+  AnimatedHeadingAnimationsEnum,
+  type AnimatedHeadingsAnimation,
+  animatedHeadinganimations,
+  lettersAnimations,
+  wholeSenteceAnimations,
 } from "./types";
 
-export interface AnimatedHeadingProps
-	extends WithClassName,
-		Pick<DynamicHeadingProps, "as"> {
-	animation: AnimatedHeadingsAnimation;
-	children: string;
-	theme?: DeepPartial<AnimateHeadingTheme>;
+export interface AnimatedHeadingProps extends WithClassName, Pick<DynamicHeadingProps, "as"> {
+  animation: AnimatedHeadingsAnimation;
+  children: string;
+  theme?: DeepPartial<AnimateHeadingTheme>;
 }
 
 /**
@@ -35,149 +32,132 @@ export interface AnimatedHeadingProps
  * @returns React.FC<AnimatedHeadingProps>
  */
 export const AnimatedHeading = ({
-	as,
-	children,
-	className,
-	theme: customTheme = {},
-	animation = AnimatedHeadingAnimationsEnum["fade-down"],
+  as,
+  children,
+  className,
+  theme: customTheme = {},
+  animation = AnimatedHeadingAnimationsEnum["fade-down"],
 }: AnimatedHeadingProps) => {
-	const theme = mergeDeep(getTheme().animatedText, customTheme);
+  const theme = mergeDeep(getTheme().animatedText, customTheme);
 
-	if (wholeSenteceAnimations.includes(animation)) {
-		return (
-			<AnimationsForWholeSentence
-				className={className}
-				animation={animatedHeadinganimations[animation]}
-				as={as}
-				theme={theme}
-			>
-				{children}
-			</AnimationsForWholeSentence>
-		);
-	}
-	// if (wordsAnimations.includes(animation)) {
-	// 	return (
-	// 		<AnimationsForWords
-	// 			animation={animatedHeadinganimations[animation]}
-	// 			as={as}
-	// 			theme={theme}
-	// 		>
-	// 			{children}
-	// 		</AnimationsForWords>
-	// 	);
-	// }
-	if (lettersAnimations.includes(animation)) {
-		return (
-			<AnimationsForLetters
-				animation={animatedHeadinganimations[animation]}
-				as={as}
-				theme={theme}
-			>
-				{children}
-			</AnimationsForLetters>
-		);
-	}
+  if (wholeSenteceAnimations.includes(animation)) {
+    return (
+      <AnimationsForWholeSentence
+        className={className}
+        animation={animatedHeadinganimations[animation]}
+        as={as}
+        theme={theme}
+      >
+        {children}
+      </AnimationsForWholeSentence>
+    );
+  }
+  // if (wordsAnimations.includes(animation)) {
+  // 	return (
+  // 		<AnimationsForWords
+  // 			animation={animatedHeadinganimations[animation]}
+  // 			as={as}
+  // 			theme={theme}
+  // 		>
+  // 			{children}
+  // 		</AnimationsForWords>
+  // 	);
+  // }
+  if (lettersAnimations.includes(animation)) {
+    return (
+      <AnimationsForLetters animation={animatedHeadinganimations[animation]} as={as} theme={theme}>
+        {children}
+      </AnimationsForLetters>
+    );
+  }
 
-	return "not implemented";
+  return "not implemented";
 };
 
-interface AnimationsItemProps
-	extends WithClassName,
-		Pick<AnimatedHeadingProps, "as" | "children"> {
-	animation: (typeof AnimatedHeading.prototype.animations)[0];
-	theme: AnimateHeadingTheme;
+interface AnimationsItemProps extends WithClassName, Pick<AnimatedHeadingProps, "as" | "children"> {
+  animation: (typeof AnimatedHeading.prototype.animations)[0];
+  theme: AnimateHeadingTheme;
 }
-export const AnimationsForWholeSentence = ({
-	animation,
-	children,
-	as,
-	className,
-	theme,
-}: AnimationsItemProps) => {
-	return (
-		<motion.div
-			initial="hidden"
-			animate="show"
-			viewport={{ once: true }}
-			variants={{
-				hidden: {},
-				show: {
-					transition: {
-						staggerChildren: 0.15,
-					},
-				},
-			}}
-		>
-			<motion.div className={cn(theme.base, className)} variants={animation}>
-				<DynamicHeading as={as}>{children}</DynamicHeading>
-			</motion.div>
-		</motion.div>
-	);
+export const AnimationsForWholeSentence = ({ animation, children, as, className, theme }: AnimationsItemProps) => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+    >
+      <motion.div className={cn(theme.base, className)} variants={animation}>
+        <DynamicHeading as={as}>{children}</DynamicHeading>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export type TextAnimation = "slide-right" | "slide-left";
 type AnimationsForWordsProps = {
-	animation?: TextAnimation;
-	text: string;
-	delay?: number;
-	separator?: string;
-	suffix?: string;
-	className?: string;
+  animation?: TextAnimation;
+  text: string;
+  delay?: number;
+  separator?: string;
+  suffix?: string;
+  className?: string;
 };
 export const AnimationsForWords = ({
-	animation = "slide-right",
-	text,
-	delay = 0.2,
-	separator = " ",
-	suffix = " ",
-	className,
+  animation = "slide-right",
+  text,
+  delay = 0.2,
+  separator = " ",
+  suffix = " ",
+  className,
 }: AnimationsForWordsProps) => {
-	const animations: Record<TextAnimation, string> = {
-		"slide-right": "animate-fade-in-right",
-		"slide-left": "animate-fade-in-left",
-	};
-	return (
-		<div>
-			{text.split(separator).map((word, idx) => (
-				<span
-					key={word}
-					style={{
-						animationDelay: `${idx * delay}s`,
-						animationFillMode: "both",
-					}}
-					className={cn(className, animations[animation])}
-				>
-					{word}
-					{suffix}
-				</span>
-			))}
-		</div>
-	);
+  const animations: Record<TextAnimation, string> = {
+    "slide-right": "animate-fade-in-right",
+    "slide-left": "animate-fade-in-left",
+  };
+  return (
+    <div>
+      {text.split(separator).map((word, idx) => (
+        <span
+          key={word}
+          style={{
+            animationDelay: `${idx * delay}s`,
+            animationFillMode: "both",
+          }}
+          className={cn(className, animations[animation])}
+        >
+          {word}
+          {suffix}
+        </span>
+      ))}
+    </div>
+  );
 };
 
-export const AnimationsForLetters = ({
-	animation,
-	children,
-	className,
-	theme,
-}: AnimationsItemProps) => {
-	return (
-		<div className="flex flex-wrap">
-			<AnimatePresence>
-				{children.split("").map((char, i) => (
-					<motion.p
-						key={char}
-						initial="hidden"
-						animate="visible"
-						exit="hidden"
-						variants={animation}
-						transition={{ duration: 0.5, delay: i * 0.1 }}
-						className={cn(theme.base, className)}
-					>
-						{char === " " ? <span>&nbsp;</span> : char}
-					</motion.p>
-				))}
-			</AnimatePresence>
-		</div>
-	);
+export const AnimationsForLetters = ({ animation, children, className, theme }: AnimationsItemProps) => {
+  return (
+    <div className="flex flex-wrap">
+      <AnimatePresence>
+        {children.split("").map((char, i) => (
+          <motion.p
+            key={char}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={animation}
+            transition={{ duration: 0.5, delay: i * 0.1 }}
+            className={cn(theme.base, className)}
+          >
+            {char === " " ? <span>&nbsp;</span> : char}
+          </motion.p>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
 };
