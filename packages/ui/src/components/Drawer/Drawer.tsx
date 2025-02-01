345f721a5946ba9664037e1d1d6b45c93b6b0dcd
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-01 20:51:27 +0100

    refactor(ui): ♻️ improve structure and styling of Drawer and FocusEffect components

--- packages/ui/src/components/Drawer/Drawer.tsx
+++ packages/ui/src/components/Drawer/Drawer.tsx
@@ -16,160 +16,129 @@ type DrawerOverlayProps = React.ComponentProps<typeof DrawerPrimitive.Overlay>;
 
 const DrawerRoot = DrawerPrimitive.Root;
 const DrawerTrigger = ({ ...props }: DrawerTriggerProps) => {
-	return <DrawerPrimitive.Trigger {...props} />;
+  return <DrawerPrimitive.Trigger {...props} />;
 };
 const DrawerPortal = ({ ...props }: DrawerPortalProps) => {
-	return <DrawerPrimitive.Portal {...props} />;
+  return <DrawerPrimitive.Portal {...props} />;
 };
 const DrawerClose = ({ ...props }: DrawerCloseProps) => {
-	return <DrawerPrimitive.Close {...props} />;
+  return <DrawerPrimitive.Close {...props} />;
 };
 
 const DrawerOverlay = ({ className, ...props }: DrawerOverlayProps) => (
-	<DrawerPrimitive.Overlay
-		className={cn("fixed inset-0 z-50 bg-black/80", className)}
-		{...props}
-	/>
+  <DrawerPrimitive.Overlay className={cn("fixed inset-0 z-50 bg-black/80", className)} {...props} />
 );
 
-interface DrawerContentProps
-	extends React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content> {
-	direction?: Direction;
-	fillBackground?: boolean;
+interface DrawerContentProps extends React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content> {
+  direction?: Direction;
+  fillBackground?: boolean;
 }
 
-const DrawerContent = React.forwardRef<
-	React.ElementRef<typeof DrawerPrimitive.Content>,
-	DrawerContentProps
->(({ className, direction, children, fillBackground, ...props }, ref) => {
-	const isVertical =
-		direction === DirectionEnum.top || direction === DirectionEnum.bottom;
-	const isHorizontal =
-		direction === DirectionEnum.right || direction === DirectionEnum.left;
+const DrawerContent = React.forwardRef<React.ElementRef<typeof DrawerPrimitive.Content>, DrawerContentProps>(
+  ({ className, direction, children, fillBackground, ...props }, ref) => {
+    const isVertical = direction === DirectionEnum.top || direction === DirectionEnum.bottom;
+    const isHorizontal = direction === DirectionEnum.right || direction === DirectionEnum.left;
 
-	return (
-		<DrawerPortal>
-			<DrawerOverlay />
-			<DrawerPrimitive.Content
-				ref={ref}
-				className={cn(
-					"fixed z-50 p-4 flex h-auto flex-col border bg-secondary-50 text-secondary-950 dark:bg-secondary-900 dark:text-secondary-50 overflow-y-auto overflow-hidden",
-					{
-						"bottom-0": direction !== DirectionEnum.top,
-						"top-0": direction !== DirectionEnum.bottom,
-						"right-0": direction !== DirectionEnum.left,
-						"left-0": direction !== DirectionEnum.right,
+    return (
+      <DrawerPortal>
+        <DrawerOverlay />
+        <DrawerPrimitive.Content
+          ref={ref}
+          className={cn(
+            "fixed z-50 p-4 flex h-auto flex-col border bg-secondary-50 text-secondary-950 dark:bg-secondary-900 dark:text-secondary-50 overflow-y-auto overflow-hidden",
+            {
+              "bottom-0": direction !== DirectionEnum.top,
+              "top-0": direction !== DirectionEnum.bottom,
+              "right-0": direction !== DirectionEnum.left,
+              "left-0": direction !== DirectionEnum.right,
 
-						"rounded-t-3xl": direction === DirectionEnum.bottom,
-						"rounded-b-3xl": direction === DirectionEnum.top,
-						"rounded-l-3xl": direction === DirectionEnum.right,
-						"rounded-r-3xl": direction === DirectionEnum.left,
+              "rounded-t-3xl": direction === DirectionEnum.bottom,
+              "rounded-b-3xl": direction === DirectionEnum.top,
+              "rounded-l-3xl": direction === DirectionEnum.right,
+              "rounded-r-3xl": direction === DirectionEnum.left,
 
-						" max-h-[96%]": isVertical,
-						" max-w-[96%]": isHorizontal,
+              " max-h-[96%]": isVertical,
+              " max-w-[96%]": isHorizontal,
 
-						"h-full": isVertical && fillBackground,
-						"w-full": isHorizontal && fillBackground,
-					},
-					className,
-				)}
-				{...props}
-			>
-				{direction === DirectionEnum.bottom && (
-					<DrawerPrimitive.Handle className="mx-auto mb-2 h-2 w-[100px] rounded-full bg-secondary" />
-				)}
-				{direction === DirectionEnum.left && (
-					<DrawerPrimitive.Close
-						className="absolute top-0 right-0 m-1"
-						aria-label="Close"
-						asChild={true}
-					>
-						<IconButton size={"sm"}>
-							<TbX size={15} />
-						</IconButton>
-					</DrawerPrimitive.Close>
-				)}
-				{direction === DirectionEnum.right && (
-					<DrawerPrimitive.Close
-						className="absolute top-0 left-0 m-1"
-						aria-label="Close"
-						asChild={true}
-					>
-						<IconButton size={"sm"}>
-							<TbX size={15} />
-						</IconButton>
-					</DrawerPrimitive.Close>
-				)}
-				<div className="overflow-y-auto rounded-xl">{children}</div>
-				{direction === DirectionEnum.top && (
-					<DrawerPrimitive.Handle className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-secondary" />
-				)}
-			</DrawerPrimitive.Content>
-		</DrawerPortal>
-	);
-});
+              "h-full": isVertical && fillBackground,
+              "w-full": isHorizontal && fillBackground,
+            },
+            className,
+          )}
+          {...props}
+        >
+          {direction === DirectionEnum.bottom && (
+            <DrawerPrimitive.Handle className="mx-auto mb-2 h-2 w-[100px] rounded-full bg-secondary" />
+          )}
+          {direction === DirectionEnum.left && (
+            <DrawerPrimitive.Close className="absolute top-0 right-0 m-1" aria-label="Close" asChild={true}>
+              <IconButton size={"sm"}>
+                <TbX size={15} />
+              </IconButton>
+            </DrawerPrimitive.Close>
+          )}
+          {direction === DirectionEnum.right && (
+            <DrawerPrimitive.Close className="absolute top-0 left-0 m-1" aria-label="Close" asChild={true}>
+              <IconButton size={"sm"}>
+                <TbX size={15} />
+              </IconButton>
+            </DrawerPrimitive.Close>
+          )}
+          <div className="overflow-y-auto rounded-xl">{children}</div>
+          {direction === DirectionEnum.top && (
+            <DrawerPrimitive.Handle className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-secondary" />
+          )}
+        </DrawerPrimitive.Content>
+      </DrawerPortal>
+    );
+  },
+);
 DrawerContent.displayName = "DrawerContent";
 
 //
 
 export type DrawerProps = React.ComponentProps<typeof DrawerPrimitive.Root> & {
-	children: React.ReactNode;
-	label?: string;
-	trigger?: React.ReactNode;
-	withoutTrigger?: boolean;
-	className?: string;
+  children: React.ReactNode;
+  label?: string;
+  trigger?: React.ReactNode;
+  withoutTrigger?: boolean;
+  className?: string;
 };
 
-const Drawer = ({
-	children,
-	label = "Open Drawer",
-	trigger,
-	withoutTrigger,
-	...props
-}: DrawerProps) => {
-	const triggerNode = trigger || <Button>{label}</Button>;
-	return (
-		<DrawerRoot {...props}>
-			{!withoutTrigger && (
-				<DrawerTrigger asChild={true}>{triggerNode}</DrawerTrigger>
-			)}
-			<DrawerContent
-				direction={props.direction ?? DirectionEnum.bottom}
-				fillBackground={props.shouldScaleBackground}
-				className={props.className}
-			>
-				{children}
-			</DrawerContent>
-		</DrawerRoot>
-	);
+const Drawer = ({ children, label = "Open Drawer", trigger, withoutTrigger, ...props }: DrawerProps) => {
+  const triggerNode = trigger || <Button>{label}</Button>;
+  return (
+    <DrawerRoot {...props}>
+      {!withoutTrigger && <DrawerTrigger asChild={true}>{triggerNode}</DrawerTrigger>}
+      <DrawerContent
+        direction={props.direction ?? DirectionEnum.bottom}
+        fillBackground={props.shouldScaleBackground}
+        className={props.className}
+      >
+        {children}
+      </DrawerContent>
+    </DrawerRoot>
+  );
 };
 
-const NestedDrawer = ({
-	children,
-	label = "Open Drawer",
-	trigger,
-	...props
-}: DrawerProps) => {
-	const triggerNode = trigger || <Button>{label}</Button>;
-	return (
-		<DrawerPrimitive.NestedRoot {...props}>
-			<DrawerTrigger>{triggerNode}</DrawerTrigger>
-			<DrawerContent
-				direction={props.direction ?? DirectionEnum.bottom}
-				fillBackground={props.shouldScaleBackground}
-			>
-				{children}
-			</DrawerContent>
-		</DrawerPrimitive.NestedRoot>
-	);
+const NestedDrawer = ({ children, label = "Open Drawer", trigger, ...props }: DrawerProps) => {
+  const triggerNode = trigger || <Button>{label}</Button>;
+  return (
+    <DrawerPrimitive.NestedRoot {...props}>
+      <DrawerTrigger>{triggerNode}</DrawerTrigger>
+      <DrawerContent direction={props.direction ?? DirectionEnum.bottom} fillBackground={props.shouldScaleBackground}>
+        {children}
+      </DrawerContent>
+    </DrawerPrimitive.NestedRoot>
+  );
 };
 export {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerOverlay,
-	DrawerPortal,
-	DrawerTrigger,
-	NestedDrawer,
-	type DrawerTriggerProps,
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTrigger,
+  NestedDrawer,
+  type DrawerTriggerProps,
 };
--- packages/ui/src/components/FocusEffect/FocusEffect.stories.tsx
+++ packages/ui/src/components/FocusEffect/FocusEffect.stories.tsx
@@ -3,15 +3,15 @@ import type { Meta } from "@storybook/react";
 import FocusEffect from "./FocusEffect";
 
 export default {
-	title: "Components/FocusEffect",
-	component: FocusEffect,
+  title: "Components/FocusEffect",
+  component: FocusEffect,
 } as Meta;
 
 export const Default = () => {
-	return (
-		<button className="group relative overflow-hidden p-4" type="button">
-			Tab me
-			<FocusEffect />
-		</button>
-	);
+  return (
+    <button className="group relative overflow-hidden p-4" type="button">
+      Tab me
+      <FocusEffect />
+    </button>
+  );
 };
