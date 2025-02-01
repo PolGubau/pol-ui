c5e444b015c9d15ed010aadd80559635b5c25b9b
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-01 11:33:32 +0100

    refactor(ui): ♻️ improve component organization for enhanced clarity

--- packages/ui/src/components/Textarea/Textarea.stories.tsx
+++ packages/ui/src/components/Textarea/Textarea.stories.tsx
@@ -6,16 +6,16 @@ import { TextArea, type TextAreaProps } from "./Textarea";
 type Story = StoryObj<typeof TextArea>;
 
 const meta: Meta<typeof TextArea> = {
-  title: "Components/Inputs/TextArea",
-  component: TextArea,
+	title: "Components/Inputs/TextArea",
+	component: TextArea,
 
-  decorators: [
-    (Story) => (
-      <div className="max-w-xl">
-        <Story />
-      </div>
-    ),
-  ],
+	decorators: [
+		(Story) => (
+			<div className="max-w-xl">
+				<Story />
+			</div>
+		),
+	],
 };
 export default meta;
 
@@ -24,60 +24,69 @@ const Template: StoryFn<TextAreaProps> = (args) => <TextArea {...args} />;
 export const Default: Story = Template.bind({});
 Default.storyName = "Text input";
 Default.args = {
-  label: "Username",
+	label: "Username",
 };
 
 export const WithIcon: Story = Template.bind({});
 WithIcon.args = {
-  label: "Username",
-  icon: TbUser,
+	label: "Username",
+	icon: TbUser,
 };
 export const RightIcon: Story = Template.bind({});
 RightIcon.args = {
-  label: "Username",
-  icon: TbUser,
-  iconPosition: "right",
+	label: "Username",
+	icon: TbUser,
+	iconPosition: "right",
 };
 export const WithLeftContent: Story = Template.bind({});
 WithLeftContent.args = {
-  label: "Username",
-  leftContent: (
-    <div className="flex">
-      <TbUser className="ml-1" />
-      <TbAlt className="ml-1" />
-    </div>
-  ),
+	label: "Username",
+	leftContent: (
+		<div className="flex">
+			<TbUser className="ml-1" />
+			<TbAlt className="ml-1" />
+		</div>
+	),
 };
+/**
+ * By default has a max-height of 192px, this is to prevent the textarea from taking up too much space. If you want to increase the height of the textarea, you can add a min-height to the classname prop or disable the max-height by setting it to max-h-full
+ */
 export const Higher: Story = Template.bind({});
 Higher.args = {
-  label: "Username",
-  className: "h-96",
+	label: "Username",
+	className: "max-h-full h-[500px]",
 };
 
 export const WithHelperText: Story = Template.bind({});
 WithHelperText.args = {
-  label: "Username",
-  helperText: "This is a helper text",
+	label: "Username",
+	helperText: "This is a helper text",
 };
 
 export const AllColors = () => (
-  <div className="flex flex-col gap-4 p-4 ">
-    {Object.keys(ColorsEnum).map((color) => (
-      <TextArea key={color} label={color} color={color as Colors} icon={TbAt} />
-    ))}
-  </div>
+	<div className="flex flex-col gap-4 p-4 ">
+		{Object.keys(ColorsEnum).map((color) => (
+			<TextArea key={color} label={color} color={color as Colors} icon={TbAt} />
+		))}
+	</div>
 );
 export const AllSizes = () => (
-  <div className="flex flex-col gap-4 p-4 ">
-    {Object.keys(MainSizesEnum).map((size) => (
-      <TextArea key={size} label={size} size={size as MainSizes} />
-    ))}
-  </div>
+	<div className="flex flex-col gap-4 p-4 ">
+		{Object.keys(MainSizesEnum).map((size) => (
+			<TextArea key={size} label={size} size={size as MainSizes} />
+		))}
+	</div>
 );
 export const AllColorsFilled = () => (
-  <div className="flex flex-col gap-4 p-4 ">
-    {Object.keys(ColorsEnum).map((color) => (
-      <TextArea key={color} label={color} color={color as Colors} icon={TbAt} defaultValue="I am in light mode" />
-    ))}
-  </div>
+	<div className="flex flex-col gap-4 p-4 ">
+		{Object.keys(ColorsEnum).map((color) => (
+			<TextArea
+				key={color}
+				label={color}
+				color={color as Colors}
+				icon={TbAt}
+				defaultValue="I am in light mode"
+			/>
+		))}
+	</div>
 );
