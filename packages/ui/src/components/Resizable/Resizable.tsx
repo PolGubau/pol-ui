56d7f515048a7d695dc4808bd22b69d5269cbd91
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-01 11:33:52 +0100

    refactor(ui): ♻️ enhance component structure for improved maintainability

--- packages/ui/src/components/Resizable/Resizable.tsx
+++ packages/ui/src/components/Resizable/Resizable.tsx
@@ -5,53 +5,47 @@ import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "../../helpers";
 export interface ResizablePanelGroupProps
-	extends Omit<
-		React.ComponentProps<typeof ResizablePrimitive.PanelGroup>,
-		"direction"
-	> {
-	direction?: "horizontal" | "vertical";
+  extends Omit<React.ComponentProps<typeof ResizablePrimitive.PanelGroup>, "direction"> {
+  direction?: "horizontal" | "vertical";
 }
 const ResizablePanelGroup = ({
-	className,
-	direction = "horizontal",
-	...props
+  className,
+  direction = "horizontal",
+  ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
-	<ResizablePrimitive.PanelGroup
-		direction={direction}
-		className={cn(
-			"flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-			className,
-		)}
-		{...props}
-	/>
+  <ResizablePrimitive.PanelGroup
+    direction={direction}
+    className={cn("flex h-full w-full data-[panel-group-direction=vertical]:flex-col", className)}
+    {...props}
+  />
 );
 
 const ResizablePanel = ResizablePrimitive.Panel;
 
 const ResizableHandle = ({
-	withHandle,
-	className,
-	customHandle: CustomHandle,
-	...props
+  withHandle,
+  className,
+  customHandle: CustomHandle,
+  ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
-	withHandle?: boolean;
-	customHandle?: React.ReactNode;
+  withHandle?: boolean;
+  customHandle?: React.ReactNode;
 }) => (
-	<ResizablePrimitive.PanelResizeHandle
-		className={cn(
-			"group relative flex w-px items-center justify-center bg-secondary/30 after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-secondary focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-			className,
-		)}
-		{...props}
-	>
-		{CustomHandle
-			? CustomHandle
-			: withHandle && (
-					<div className="z-10 rounded-full flex h-10 w-fit items-center justify-center bg-secondary-200 dark:bg-secondary-400 text-background">
-						<TbDotsVertical className="h-6 w-4" />
-					</div>
-				)}
-	</ResizablePrimitive.PanelResizeHandle>
+  <ResizablePrimitive.PanelResizeHandle
+    className={cn(
+      "group relative flex w-px items-center justify-center bg-secondary/30 after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-secondary focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      className,
+    )}
+    {...props}
+  >
+    {CustomHandle
+      ? CustomHandle
+      : withHandle && (
+          <div className="z-10 rounded-full flex h-10 w-fit items-center justify-center bg-secondary-200 dark:bg-secondary-400 text-background">
+            <TbDotsVertical className="h-6 w-4" />
+          </div>
+        )}
+  </ResizablePrimitive.PanelResizeHandle>
 );
 
 export { ResizableHandle, ResizablePanel, ResizablePanelGroup };
