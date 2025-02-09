de50222b31cf0a1076497b294aae8150c6ce253e
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-09 17:06:36 +0100

    refactor(ui): ♻️ update package dependencies and improve formatting

--- packages/ui/src/components/DebouncedInput/DebouncedInput.tsx
+++ packages/ui/src/components/DebouncedInput/DebouncedInput.tsx
@@ -6,10 +6,11 @@ import { useDebounce } from "../../hooks";
 import { Input } from "../Input/Input";
 import type { InputProps } from "../Input/props";
 
-export interface DebouncedInputProps extends Omit<InputProps, "onChange" | "value"> {
-  delay?: number;
-  onChange: (value: string) => void;
-  value: string;
+export interface DebouncedInputProps
+	extends Omit<InputProps, "onChange" | "value"> {
+	delay?: number;
+	onChange: (value: string) => void;
+	value: string;
 }
 
 /**
@@ -18,39 +19,39 @@ export interface DebouncedInputProps extends Omit<InputProps, "onChange" | "valu
  * @returns React.FC<DebouncedInputProps>
  */
 export const DebouncedInput = ({
-  value: propValue,
-  onChange: propOnChange,
-  delay = 500,
-  ref,
-  ...props
+	value: propValue,
+	onChange: propOnChange,
+	delay = 500,
+	ref,
+	...props
 }: DebouncedInputProps) => {
-  const [inputValue, setInputValue] = useState<string>(propValue);
-  const debouncedValue = useDebounce<string>(inputValue, delay);
+	const [inputValue, setInputValue] = useState<string>(propValue);
+	const debouncedValue = useDebounce<string>(inputValue, delay);
 
-  useEffect(() => {
-    // Pass the debounced value to the propOnChange after the specified delay
-    const timer = setTimeout(() => {
-      propOnChange(debouncedValue);
-    }, delay);
+	useEffect(() => {
+		// Pass the debounced value to the propOnChange after the specified delay
+		const timer = setTimeout(() => {
+			propOnChange(debouncedValue);
+		}, delay);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [debouncedValue, propOnChange, delay]);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [debouncedValue, propOnChange, delay]);
 
-  // Update local state when the propValue changes (e.g., parent re-renders)
-  useEffect(() => {
-    setInputValue(propValue);
-  }, [propValue]);
+	// Update local state when the propValue changes (e.g., parent re-renders)
+	useEffect(() => {
+		setInputValue(propValue);
+	}, [propValue]);
 
-  return (
-    <Input
-      {...props}
-      value={inputValue}
-      onChange={(e) => {
-        setInputValue(e.target.value);
-      }}
-      ref={ref}
-    />
-  );
+	return (
+		<Input
+			{...props}
+			value={inputValue}
+			onChange={(e) => {
+				setInputValue(e.target.value);
+			}}
+			ref={ref}
+		/>
+	);
 };
