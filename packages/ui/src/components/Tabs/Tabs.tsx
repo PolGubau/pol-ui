a54d6f7d79f2026c09564bdcbcbb1d0776d5a847
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-15 18:58:43 +0100

    refactor(ui): ♻️ improve formatting and consistency in Tabs and useMergeValue components

--- packages/ui/src/components/Tabs/Tabs.tsx
+++ packages/ui/src/components/Tabs/Tabs.tsx
@@ -9,106 +9,124 @@ import { getTheme } from "../../theme-store";
 import type { TabsTheme } from "./theme";
 
 type Tab = {
-  name: string;
-  content?: React.ReactNode;
-  disabled?: boolean;
+	name: string;
+	content?: React.ReactNode;
+	disabled?: boolean;
 };
 
 export enum TabMode {
-  underlined = "underlined",
-  contained = "contained",
+	underlined = "underlined",
+	contained = "contained",
 }
 export type TabModeType = keyof typeof TabMode;
 
 export interface TabsProps {
-  tabs: Tab[];
-  containerClassName?: string;
-  activeTabClassName?: string;
-  tabClassName?: string;
-  contentClassName?: string;
-  hasNavMotion?: boolean;
-  mode?: TabModeType;
-  theme?: Partial<TabsTheme>;
-  value?: number;
-  defaultValue?: number;
-  onTabChange?: (index: number) => void;
+	tabs: Tab[];
+	containerClassName?: string;
+	activeTabClassName?: string;
+	tabClassName?: string;
+	contentClassName?: string;
+	hasNavMotion?: boolean;
+	mode?: TabModeType;
+	theme?: Partial<TabsTheme>;
+	value?: number;
+	defaultValue?: number;
+	onTabChange?: (index: number) => void;
 }
 
 export const Tabs: React.FC<TabsProps> = ({
-  tabs: propTabs,
-  containerClassName,
-  activeTabClassName,
-  tabClassName,
-  contentClassName,
-  hasNavMotion = true,
-  mode = TabMode.underlined,
-  theme: customTheme = {},
-  value,
-  defaultValue = 0,
-  onTabChange,
+	tabs: propTabs,
+	containerClassName,
+	activeTabClassName,
+	tabClassName,
+	contentClassName,
+	hasNavMotion = true,
+	mode = TabMode.underlined,
+	theme: customTheme = {},
+	value,
+	defaultValue = 0,
+	onTabChange,
 }: TabsProps) => {
-  if (propTabs.length === 0) {
-    throw new Error("Tabs must have at least one tab");
-  }
+	if (propTabs.length === 0) {
+		throw new Error("Tabs must have at least one tab");
+	}
 
-  const [activeIdx, setActiveIdx] = useMergeValue(defaultValue, {
-    value,
-    onChange: onTabChange,
-  });
+	const [activeIdx, setActiveIdx] = useMergeValue(defaultValue, {
+		value,
+		onChange: onTabChange,
+	});
 
-  const theme: TabsTheme = mergeDeep(getTheme().tabs, customTheme);
-  const id = useId();
-  const isActive = (idx: number) => activeIdx === idx;
-  const tab = propTabs[activeIdx];
-  return (
-    <div className="flex flex-col gap-4">
-      <ul className={cn(theme.base, containerClassName)}>
-        {propTabs.map((tab) => {
-          const idx = propTabs.indexOf(tab);
-          const isThisActive = isActive(idx);
-          const setThisActive = () => setActiveIdx(idx);
-          return (
-            <button
-              type="button"
-              disabled={tab.disabled}
-              key={tab.name}
-              role="tab"
-              aria-selected={isThisActive}
-              onClick={() => {
-                onTabChange?.(idx);
-                setThisActive();
-              }}
-              className={cn(theme.navItem?.base, tab.disabled && theme.disabled, tabClassName)}
-            >
-              {isThisActive && (
-                <motion.div
-                  layoutId={hasNavMotion ? id : undefined}
-                  transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
-                  className={cn(
-                    theme.navItem.marker?.base,
-                    theme.navItem.marker?.active.on,
-                    theme.navItem.marker?.mode[mode],
-                    activeTabClassName,
-                  )}
-                />
-              )}
+	const theme: TabsTheme = mergeDeep(getTheme().tabs, customTheme);
+	const id = useId();
+	const isActive = (idx: number) => activeIdx === idx;
+	const tab = propTabs[activeIdx];
 
-              <span className={cn(theme.navItem.text)}>{tab.name}</span>
-            </button>
-          );
-        })}
-      </ul>
-      {tab && <TabContent content={tab.content} key={tab.name} className={cn("mt-4", contentClassName)} />}{" "}
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4">
+			<ul className={cn(theme.base, containerClassName)}>
+				{propTabs.map((tab) => {
+					const idx = propTabs.indexOf(tab);
+					const isThisActive = isActive(idx);
+					const setThisActive = () => setActiveIdx(idx);
+					return (
+						<button
+							type="button"
+							disabled={tab.disabled}
+							key={tab.name}
+							role="tab"
+							aria-selected={isThisActive}
+							onClick={() => {
+								onTabChange?.(idx);
+								setThisActive();
+							}}
+							className={cn(
+								theme.navItem?.base,
+								tab.disabled && theme.disabled,
+								tabClassName,
+							)}
+						>
+							{isThisActive && (
+								<motion.div
+									layoutId={hasNavMotion ? id : undefined}
+									transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
+									className={cn(
+										theme.navItem.marker?.base,
+										theme.navItem.marker?.active.on,
+										theme.navItem.marker?.mode[mode],
+										activeTabClassName,
+									)}
+								/>
+							)}
+
+							<span className={cn(theme.navItem.text)}>{tab.name}</span>
+						</button>
+					);
+				})}
+			</ul>
+			{activeIdx}
+			{tab && (
+				<TabContent
+					content={tab.content}
+					key={tab.name}
+					className={cn("mt-4", contentClassName)}
+				/>
+			)}
+		</div>
+	);
 };
 
 export const TabContent = ({
-  className,
-  content,
+	className,
+	content,
 }: {
-  className?: string;
-  content: Tab["content"];
+	className?: string;
+	content: Tab["content"];
 }) => {
-  return <motion.div className={cn("relative h-full w-full transition-all ", className)}>{content}</motion.div>;
+	return (
+		<motion.div
+			className={cn("relative h-full w-full transition-all ", className)}
+		>
+			{content}
+		</motion.div>
+	);
 };
--- packages/ui/src/hooks/use-merge-value/use-merge-value.stories.tsx
+++ packages/ui/src/hooks/use-merge-value/use-merge-value.stories.tsx
@@ -3,90 +3,115 @@ import { useState } from "react";
 import { Button, Input } from "../../components";
 import { useMergeValue } from "./use-merge-value";
 const meta: Meta = {
-  title: "Hooks/useMergeValue",
-  parameters: {
-    layout: "fullscreen",
-  },
-  decorators: [
-    (Story) => (
-      <div className="flex p-6 flex-col justify-center items-center ">
-        <div className="max-w-xl">
-          <Story />
-        </div>
-      </div>
-    ),
-  ],
+	title: "Hooks/useMergeValue",
+	parameters: {
+		layout: "fullscreen",
+	},
+	decorators: [
+		(Story) => (
+			<div className="flex p-6 flex-col justify-center items-center ">
+				<div className="max-w-xl">
+					<Story />
+				</div>
+			</div>
+		),
+	],
 };
 export default meta;
 
+const ControlledInput = ({
+	value,
+	onChange,
+}: { value?: string; onChange?: (value: string) => void }) => {
+	const [inputValue, setInputValue] = useMergeValue("", {
+		value,
+		onChange,
+	});
+
+	return (
+		<Input
+			value={inputValue}
+			onChange={(e) => setInputValue(e.target.value)}
+			label="Type something..."
+		/>
+	);
+};
 export const Default: StoryObj = {
-  render: () => {
-    const ControlledInput = ({ value, onChange }: { value?: string; onChange?: (value: string) => void }) => {
-      const [inputValue, setInputValue] = useMergeValue("", {
-        value,
-        onChange,
-      });
-
-      return <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} label="Type something..." />;
-    };
-
-    const [value, setValue] = useState<string | undefined>("Controlled Value");
-    return (
-      <div className="flex flex-col gap-4">
-        <h3 className="text-lg font-semibold">Controlled Component</h3>
-        <ControlledInput value={value} onChange={setValue} />
-        <Button
-          onClick={() => setValue((prev) => (prev === "Controlled Value" ? "Updated Value" : "Controlled Value"))}
-        >
-          Toggle Controlled Value
-        </Button>
-      </div>
-    );
-  },
+	render: () => {
+		const [value, setValue] = useState<string | undefined>("Controlled Value");
+		return (
+			<div className="flex flex-col gap-4">
+				<h3 className="text-lg font-semibold">Controlled Component</h3>
+				<ControlledInput value={value} onChange={setValue} />
+				<Button
+					onClick={() =>
+						setValue((prev) =>
+							prev === "Controlled Value"
+								? "Updated Value"
+								: "Controlled Value",
+						)
+					}
+				>
+					Toggle Controlled Value
+				</Button>
+			</div>
+		);
+	},
 };
 
 export const Uncontrolled: StoryObj = {
-  render: () => {
-    const UncontrolledInput = () => {
-      const [inputValue, setInputValue] = useMergeValue("Default Value");
+	render: () => {
+		const UncontrolledInput = () => {
+			const [inputValue, setInputValue] = useMergeValue("Default Value");
 
-      return <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} label="Type something..." />;
-    };
+			return (
+				<Input
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					label="Type something..."
+				/>
+			);
+		};
 
-    return (
-      <div className="flex flex-col gap-4">
-        <h3 className="text-lg font-semibold">Uncontrolled Component</h3>
-        <UncontrolledInput />
-      </div>
-    );
-  },
+		return (
+			<div className="flex flex-col gap-4">
+				<h3 className="text-lg font-semibold">Uncontrolled Component</h3>
+				<UncontrolledInput />
+			</div>
+		);
+	},
 };
 
 export const ToggleControlledToUncontrolled: StoryObj = {
-  render: () => {
-    const FlexibleInput = ({ controlled }: { controlled: boolean }) => {
-      const [value, setValue] = useMergeValue("Default Value", controlled ? { value: "Controlled Value" } : {});
+	render: () => {
+		const FlexibleInput = ({ controlled }: { controlled: boolean }) => {
+			const [value, setValue] = useMergeValue(
+				"Default Value",
+				controlled ? { value: "Controlled Value" } : {},
+			);
 
-      return (
-        <Input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          label={controlled ? "Controlled" : "Uncontrolled"}
-        />
-      );
-    };
+			return (
+				<Input
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					label={controlled ? "Controlled" : "Uncontrolled"}
+				/>
+			);
+		};
 
-    const [isControlled, setIsControlled] = useState(true);
+		const [isControlled, setIsControlled] = useState(true);
 
-    return (
-      <div className="flex flex-col gap-4">
-        <h3 className="text-lg font-semibold">Toggle Between Controlled and Uncontrolled</h3>
-        <FlexibleInput controlled={isControlled} />
-        {/*  */}
-        <Button onClick={() => setIsControlled((prev) => !prev)}>
-          Toggle Mode ({isControlled ? "Controlled" : "Uncontrolled"})
-        </Button>
-      </div>
-    );
-  },
+		return (
+			<div className="flex flex-col gap-4">
+				<h3 className="text-lg font-semibold">
+					Toggle Between Controlled and Uncontrolled
+				</h3>
+				<FlexibleInput controlled={isControlled} />
+				{/*  */}
+				<Button onClick={() => setIsControlled((prev) => !prev)}>
+					Toggle Mode ({isControlled ? "Controlled" : "Uncontrolled"})
+				</Button>
+			</div>
+		);
+	},
 };
