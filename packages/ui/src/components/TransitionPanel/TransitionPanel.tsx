0ae362746d2d45f1bbeb693be542a15cfd98e61f
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-16 11:10:28 +0100

    refactor(ui): ♻️ improve formatting and readability in TransitionPanel component

--- packages/ui/src/components/TransitionPanel/TransitionPanel.tsx
+++ packages/ui/src/components/TransitionPanel/TransitionPanel.tsx
@@ -1,40 +1,50 @@
 "use client";
 
-import { AnimatePresence, type MotionProps, type Transition, type Variant, motion } from "framer-motion";
+import {
+	AnimatePresence,
+	type MotionProps,
+	type Transition,
+	type Variant,
+	motion,
+} from "framer-motion";
 
 import { cn } from "../../helpers";
 
 export type TransitionPanelProps = {
-  children: React.ReactNode[];
-  className?: string;
-  transition?: Transition;
-  activeIndex: number;
-  variants?: { enter: Variant; center: Variant; exit: Variant };
+	children: React.ReactNode[];
+	className?: string;
+	transition?: Transition;
+	activeIndex: number;
+	variants?: { enter: Variant; center: Variant; exit: Variant };
 } & MotionProps;
 
 export function TransitionPanel({
-  children,
-  className,
-  transition,
-  variants,
-  activeIndex,
-  ...motionProps
+	children,
+	className,
+	transition,
+	variants,
+	activeIndex,
+	...motionProps
 }: TransitionPanelProps) {
-  return (
-    <div className={cn("relative", className)}>
-      <AnimatePresence initial={false} mode="popLayout" custom={motionProps.custom}>
-        <motion.div
-          key={activeIndex}
-          variants={variants}
-          transition={transition}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          {...motionProps}
-        >
-          {children[activeIndex]}
-        </motion.div>
-      </AnimatePresence>
-    </div>
-  );
+	return (
+		<div className={cn("relative", className)}>
+			<AnimatePresence
+				initial={false}
+				mode="popLayout"
+				custom={motionProps.custom}
+			>
+				<motion.div
+					key={activeIndex}
+					variants={variants}
+					transition={transition}
+					initial="enter"
+					animate="center"
+					exit="exit"
+					{...motionProps}
+				>
+					{children[activeIndex]}
+				</motion.div>
+			</AnimatePresence>
+		</div>
+	);
 }
