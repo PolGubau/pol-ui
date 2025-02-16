1e2a9be00e910388f3b8070dc65b912b45cd1c25
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-16 10:57:49 +0100

    refactor(ui): ♻️ enhance BooleanCard and Message components for improved readability and structure

--- packages/ui/src/components/boolean-card/BooleanCard.stories.tsx
+++ packages/ui/src/components/boolean-card/BooleanCard.stories.tsx
@@ -5,53 +5,65 @@ import { useBoolean } from "../../hooks";
 import { BooleanCard, BooleanLoadingCard } from "./index";
 
 const meta: Meta<typeof BooleanCard> = {
-  title: "Components/BooleanCard",
+	title: "Components/BooleanCard",
 
-  decorators: [
-    (Story) => (
-      <div className="max-w-md w-full p-4 bg-background-primary">
-        <Story />
-      </div>
-    ),
-  ],
-  component: BooleanCard,
+	decorators: [
+		(Story) => (
+			<div className="max-w-md w-full p-4">
+				<Story />
+			</div>
+		),
+	],
+	component: BooleanCard,
 };
 export default meta;
 
 export const Default = () => {
-  return <BooleanCard title="Remember me" description="Your account will be saved" />;
+	return (
+		<BooleanCard title="Remember me" description="Your account will be saved" />
+	);
 };
 
 export const LongCard = () => {
-  return (
-    <div className="w-full">
-      <BooleanCard title="Remember me" description="Your account will be saved" />
-    </div>
-  );
+	return (
+		<div className="w-full">
+			<BooleanCard
+				title="Remember me"
+				description="Your account will be saved"
+			/>
+		</div>
+	);
 };
 
 export const Loading = () => {
-  const { value, toggle } = useBoolean(true);
+	const { value, toggle } = useBoolean(true);
 
-  return (
-    <div className="flex flex-col gap-4">
-      {value ? <BooleanLoadingCard /> : <BooleanCard title="Remember me" description="Your account will be saved" />}
+	return (
+		<div className="flex flex-col gap-4">
+			{value ? (
+				<BooleanLoadingCard />
+			) : (
+				<BooleanCard
+					title="Remember me"
+					description="Your account will be saved"
+				/>
+			)}
 
-      <button onClick={toggle} type="button">
-        Toggle loading
-      </button>
-    </div>
-  );
+			<button onClick={toggle} type="button">
+				Toggle loading
+			</button>
+		</div>
+	);
 };
 export const LongTitle = () => {
-  return (
-    <div className="w-full ">
-      <BooleanCard
-        title="Remember me when logging in again in this mobile (IPhone 13 max)"
-        description="Your account will be saved"
-      />
-    </div>
-  );
+	return (
+		<div className="w-full ">
+			<BooleanCard
+				title="Remember me when logging in again in this mobile (IPhone 13 max)"
+				description="Your account will be saved"
+			/>
+		</div>
+	);
 };
 
 /**
@@ -59,81 +71,88 @@ export const LongTitle = () => {
  * You can see the full description
  */
 export const LongDescription = () => {
-  return (
-    <div className="w-full ">
-      <BooleanCard
-        title="Remember me when logging in again in this mobile (IPhone 13 max)"
-        description="EN UN LUGAR de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de   -fol. 1v-   lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque, por conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad."
-      />
-    </div>
-  );
+	return (
+		<div className="w-full ">
+			<BooleanCard
+				title="Remember me when logging in again in this mobile (IPhone 13 max)"
+				description="EN UN LUGAR de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de   -fol. 1v-   lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque, por conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad."
+			/>
+		</div>
+	);
 };
 
 /**
  * Long description cut with line-clamp-3
  */
 export const LongDescriptionCut = () => {
-  return (
-    <div className="w-full ">
-      <BooleanCard
-        descriptionProps={{
-          className: "line-clamp-3",
-        }}
-        title="Remember me when logging in again in this mobile (IPhone 13 max)"
-        description="EN UN LUGAR de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de   -fol. 1v-   lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque, por conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad."
-      />
-    </div>
-  );
+	return (
+		<div className="w-full ">
+			<BooleanCard
+				descriptionProps={{
+					className: "line-clamp-3",
+				}}
+				title="Remember me when logging in again in this mobile (IPhone 13 max)"
+				description="EN UN LUGAR de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de   -fol. 1v-   lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque, por conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad."
+			/>
+		</div>
+	);
 };
 
 export const Controlled = () => {
-  const [checked, setChecked] = React.useState(false);
+	const [checked, setChecked] = React.useState(false);
 
-  return (
-    <div className="w-full flex flex-col gap-2">
-      <header className="flex gap-2 items-center">
-        <div
-          className={cn("w-4 h-4 rounded-full", {
-            "bg-primary": checked,
-            "bg-error": !checked,
-          })}
-        />
-        {checked ? "Checked" : "Unchecked"}
-      </header>
-      <BooleanCard
-        title="Remember me"
-        description="Your account will be saved"
-        checked={checked}
-        onChange={setChecked}
-      />
-    </div>
-  );
+	return (
+		<div className="w-full flex flex-col gap-2">
+			<header className="flex gap-2 items-center">
+				<div
+					className={cn("w-4 h-4 rounded-full", {
+						"bg-primary": checked,
+						"bg-error": !checked,
+					})}
+				/>
+				{checked ? "Checked" : "Unchecked"}
+			</header>
+			<BooleanCard
+				title="Remember me"
+				description="Your account will be saved"
+				checked={checked}
+				onChange={setChecked}
+			/>
+		</div>
+	);
 };
 
 export const CustomDescription = () => {
-  const [checked, setChecked] = React.useState(false);
+	const [checked, setChecked] = React.useState(false);
 
-  const FunkyDescription = () => {
-    return (
-      <div className="flex gap-2 flex-col">
-        <span>Custom description</span>
-        <span className="text-xs text-secondary">This is a custom description</span>
-        <div className="flex gap-2 items-center">
-          <div
-            className={cn("w-4 h-4 rounded-full", {
-              "bg-primary": checked,
-              "bg-error": !checked,
-            })}
-          />
-          {checked ? "Checked" : "Unchecked"}
-        </div>
-      </div>
-    );
-  };
+	const FunkyDescription = () => {
+		return (
+			<div className="flex gap-2 flex-col">
+				<span>Custom description</span>
+				<span className="text-xs text-secondary">
+					This is a custom description
+				</span>
+				<div className="flex gap-2 items-center">
+					<div
+						className={cn("w-4 h-4 rounded-full", {
+							"bg-primary": checked,
+							"bg-error": !checked,
+						})}
+					/>
+					{checked ? "Checked" : "Unchecked"}
+				</div>
+			</div>
+		);
+	};
 
-  return (
-    <div className="w-full flex flex-col gap-2">
-      <BooleanCard title="Remember me" description={<FunkyDescription />} checked={checked} onChange={setChecked} />
-    </div>
-  );
+	return (
+		<div className="w-full flex flex-col gap-2">
+			<BooleanCard
+				title="Remember me"
+				description={<FunkyDescription />}
+				checked={checked}
+				onChange={setChecked}
+			/>
+		</div>
+	);
 };
--- packages/ui/src/components/boolean-card/booleanLoadingCard.tsx
+++ packages/ui/src/components/boolean-card/booleanLoadingCard.tsx
@@ -1,17 +1,35 @@
+import { cn } from "../../helpers";
 import { Card } from "../Card";
 import { Switch } from "../Switch/Switch";
 
-export const BooleanLoadingCard = () => {
-  return (
-    <Card className="opacity-60 animate-pulse">
-      <div className="flex justify-between gap-4 w-full">
-        <header className="flex flex-col gap-1 w-full">
-          <h4 className="bg-secondary/40 w-fit rounded-full text-transparent">lorem pisum lorem</h4>
-          <h5 className="text-sm w-fit bg-secondary/20 rounded-full text-transparent">lorem pisum lorem epson</h5>
-        </header>
+interface BooleanLoadingCardProps {
+	defaultChecked?: boolean;
+	title: string;
+	description?: string;
+}
+export const BooleanLoadingCard = (props: BooleanLoadingCardProps) => {
+	const { defaultChecked = false, title, description } = props;
+	return (
+		<Card
+			className={cn(
+				"cursor-pointer transition-colors h-fit bg-secondary/5 animate-pulse ",
+				{
+					"bg-secondary/15": defaultChecked,
+				},
+			)}
+		>
+			<div className="flex justify-between gap-4 w-full">
+				<header className="flex flex-col gap-1 w-full">
+					<h4 className="bg-secondary/40 w-fit rounded-full text-transparent">
+						{title || "Title of the card"}
+					</h4>
+					<h5 className="text-sm w-fit bg-secondary/20 rounded-full text-transparent">
+						{description || "Description of a loading card"}
+					</h5>
+				</header>
 
-        <Switch disabled={true} checked={false} onChange={() => null} />
-      </div>
-    </Card>
-  );
+				<Switch disabled={true} checked={false} onChange={() => null} />
+			</div>
+		</Card>
+	);
 };
