66a16660b8bd20b809494a7ba6629f23e3927e7f
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-16 10:58:45 +0100

    refactor(ui): ♻️ enhance structure and readability in components

--- packages/ui/src/components/boolean-card/booleanLoadingCard.tsx
+++ packages/ui/src/components/boolean-card/booleanLoadingCard.tsx
@@ -3,37 +3,26 @@ import { Card } from "../Card";
 import { Switch } from "../Switch/Switch";
 
 interface BooleanLoadingCardProps {
-	defaultChecked?: boolean;
-	title?: string;
-	description?: string;
+  defaultChecked?: boolean;
+  title?: string;
+  description?: string;
 }
 export const BooleanLoadingCard = (props: BooleanLoadingCardProps) => {
-	const {
-		defaultChecked = false,
-		title = "Title of the card",
-		description = "Description of a loading card",
-	} = props;
-	return (
-		<Card
-			className={cn(
-				"cursor-pointer transition-colors h-fit bg-secondary/5 animate-pulse ",
-				{
-					"bg-secondary/15": defaultChecked,
-				},
-			)}
-		>
-			<div className="flex justify-between gap-4 w-full">
-				<header className="flex flex-col gap-1 w-full">
-					<h4 className="bg-secondary/40 w-fit rounded-full text-transparent">
-						{title}
-					</h4>
-					<h5 className="text-sm w-fit bg-secondary/20 rounded-full text-transparent">
-						{description}
-					</h5>
-				</header>
+  const { defaultChecked = false, title = "Title of the card", description = "Description of a loading card" } = props;
+  return (
+    <Card
+      className={cn("cursor-pointer transition-colors h-fit bg-secondary/5 animate-pulse ", {
+        "bg-secondary/15": defaultChecked,
+      })}
+    >
+      <div className="flex justify-between gap-4 w-full">
+        <header className="flex flex-col gap-1 w-full">
+          <h4 className="bg-secondary/40 w-fit rounded-full text-transparent">{title}</h4>
+          <h5 className="text-sm w-fit bg-secondary/20 rounded-full text-transparent">{description}</h5>
+        </header>
 
-				<Switch disabled={true} checked={false} onChange={() => null} />
-			</div>
-		</Card>
-	);
+        <Switch disabled={true} checked={false} onChange={() => null} />
+      </div>
+    </Card>
+  );
 };
