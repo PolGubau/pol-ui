9fe632219126ddbf04df29d44f4207f7a8ec77e0
Pol Gubau Amores <gubaupol@gmail.com>
2025-02-16 17:42:22 +0100

    refactor(ui): ♻️ improve formatting and readability in Tooltip and Input components

--- packages/ui/src/components/Input/props.ts
+++ packages/ui/src/components/Input/props.ts
@@ -8,8 +8,8 @@ import type { InputTheme } from "./theme";
  * @enum [top, left]
  */
 export enum InputLabelPositionsEnum {
-  top = "top",
-  left = "left",
+	top = "top",
+	left = "left",
 }
 
 /**
@@ -24,144 +24,143 @@ export type InputLabelPositions = keyof typeof InputLabelPositionsEnum;
  * @description Base props for input components.
  */
 export interface BaseInputsProps {
-  ref?: React.Ref<HTMLInputElement>;
-  /**
-   * @name color
-   * @description Optional prop to specify the color of the input.
-   * @default null
-   * @type Colors
-   * @enum [primary, secondary, success, danger, warning, info, light, dark, link]
-   * @example
-   * ```tsx
-   * <BaseInputs color="primary" />
-   * ```
-   */
-  color?: Colors;
+	/**
+	 * @name color
+	 * @description Optional prop to specify the color of the input.
+	 * @default null
+	 * @type Colors
+	 * @enum [primary, secondary, success, danger, warning, info, light, dark, link]
+	 * @example
+	 * ```tsx
+	 * <BaseInputs color="primary" />
+	 * ```
+	 */
+	color?: Colors;
 
-  /**
-   * @name helperText
-   * @description Optional prop to provide helper text for the input.
-   * @default null
-   * @type ReactNode
-   * @example
-   * ```tsx
-   * <BaseInputs helperText="This is a helper text" />
-   * ```
-   */
-  helperText?: ReactNode;
+	/**
+	 * @name helperText
+	 * @description Optional prop to provide helper text for the input.
+	 * @default null
+	 * @type ReactNode
+	 * @example
+	 * ```tsx
+	 * <BaseInputs helperText="This is a helper text" />
+	 * ```
+	 */
+	helperText?: ReactNode;
 
-  /**
-   * @name icon
-   * @description Optional prop to add a component to the right of the input. Recommended to icons
-   * @default null
-   * @type ReactNode
-   * @example
-   * ```tsx
-   * <BaseInputs icon={<RandomIcon />} />
-   * ```
-   */
-  icon?: React.FC<ComponentProps<"svg">>;
+	/**
+	 * @name icon
+	 * @description Optional prop to add a component to the right of the input. Recommended to icons
+	 * @default null
+	 * @type ReactNode
+	 * @example
+	 * ```tsx
+	 * <BaseInputs icon={<RandomIcon />} />
+	 * ```
+	 */
+	icon?: React.FC<ComponentProps<"svg">>;
 
-  /**
-   * @name size
-   * @description Optional prop to specify the size of the input.
-   * @default null
-   * @type MainSizes
-   * @enum [xs, sm, md, lg, xl]
-   * @example
-   * ```tsx
-   * <BaseInputs size="md" />
-   * ```
-   */
-  size?: MainSizes;
+	/**
+	 * @name size
+	 * @description Optional prop to specify the size of the input.
+	 * @default null
+	 * @type MainSizes
+	 * @enum [xs, sm, md, lg, xl]
+	 * @example
+	 * ```tsx
+	 * <BaseInputs size="md" />
+	 * ```
+	 */
+	size?: MainSizes;
 
-  /**
-   * @name theme
-   * @description Optional prop to change the theme of the input.
-   * @default {}
-   * @type DeepPartial<InputTheme>
-   * @example
-   * ```tsx
-   * <BaseInputs theme={{ color: { primary: 'bg-red-500' } }} />
-   * ```
-   */
-  theme?: DeepPartial<InputTheme>;
+	/**
+	 * @name theme
+	 * @description Optional prop to change the theme of the input.
+	 * @default {}
+	 * @type DeepPartial<InputTheme>
+	 * @example
+	 * ```tsx
+	 * <BaseInputs theme={{ color: { primary: 'bg-red-500' } }} />
+	 * ```
+	 */
+	theme?: DeepPartial<InputTheme>;
 
-  /**
-   * @name label
-   * @description Optional prop to add a label to the input.
-   * @default null
-   * @type string
-   * @example
-   * ```tsx
-   * <BaseInputs label="Username" />
-   * ```
-   */
-  label: string;
+	/**
+	 * @name label
+	 * @description Optional prop to add a label to the input.
+	 * @default null
+	 * @type string
+	 * @example
+	 * ```tsx
+	 * <BaseInputs label="Username" />
+	 * ```
+	 */
+	label: string;
 
-  /**
-   * @name labelClassName
-   * @description Optional prop to add additional classes to the label.
-   * @default ''
-   * @type string
-   * @example
-   * ```tsx
-   * <BaseInputs label="Username" labelClassName="text-red-500" />
-   * ```
-   */
-  labelClassName?: string;
+	/**
+	 * @name labelClassName
+	 * @description Optional prop to add additional classes to the label.
+	 * @default ''
+	 * @type string
+	 * @example
+	 * ```tsx
+	 * <BaseInputs label="Username" labelClassName="text-red-500" />
+	 * ```
+	 */
+	labelClassName?: string;
 
-  /**
-   * @name innerClassName
-   * @description Optional prop to add additional classes to the inner input container.
-   * @default ''
-   * @type string
-   * @example
-   * ```tsx
-   * <BaseInputs innerClassName="border-red-500" />
-   * ```
-   */
-  innerClassName?: string;
+	/**
+	 * @name innerClassName
+	 * @description Optional prop to add additional classes to the inner input container.
+	 * @default ''
+	 * @type string
+	 * @example
+	 * ```tsx
+	 * <BaseInputs innerClassName="border-red-500" />
+	 * ```
+	 */
+	innerClassName?: string;
 
-  /**
-   * @name required
-   * @description Optional prop to specify if the input is required.
-   * @default false
-   * @type boolean
-   * @example
-   * ```tsx
-   * <BaseInputs required />
-   * ```
-   */
-  required?: boolean;
+	/**
+	 * @name required
+	 * @description Optional prop to specify if the input is required.
+	 * @default false
+	 * @type boolean
+	 * @example
+	 * ```tsx
+	 * <BaseInputs required />
+	 * ```
+	 */
+	required?: boolean;
 
-  /**
-   * @name error
-   * @description Optional prop to specify if the input has an error.
-   * @default undefined
-   * @type string
-   * @example
-   * ```tsx
-   * <Input label="hola" error="This is an error" />
-   * ```
-   */
-  error?: string;
+	/**
+	 * @name error
+	 * @description Optional prop to specify if the input has an error.
+	 * @default undefined
+	 * @type string
+	 * @example
+	 * ```tsx
+	 * <Input label="hola" error="This is an error" />
+	 * ```
+	 */
+	error?: string;
 
-  rightContent?: ReactNode;
-  leftContent?: ReactNode;
+	rightContent?: ReactNode;
+	leftContent?: ReactNode;
 
-  iconPosition?: "left" | "right";
+	iconPosition?: "left" | "right";
 
-  //
-  /**
-   * @description Callback function that is triggered when the input value has stopped changing
-   */
-  onChangeEnd?: (value: string) => void;
+	//
+	/**
+	 * @description Callback function that is triggered when the input value has stopped changing
+	 */
+	onChangeEnd?: (value: string) => void;
 
-  /**
-   * @description Callback function that is triggered when the input value changes
-   */
-  onChangeValue?: (value: string) => void;
+	/**
+	 * @description Callback function that is triggered when the input value changes
+	 */
+	onChangeValue?: (value: string) => void;
 }
 
 /**
@@ -171,5 +170,10 @@ export interface BaseInputsProps {
  * @property addon `ReactNode`
  */
 export interface InputProps
-  extends Omit<ComponentProps<"input">, "ref" | "color" | "size" | "placeholder">,
-    BaseInputsProps {}
+	extends Omit<
+			ComponentProps<"input">,
+			"ref" | "color" | "size" | "placeholder"
+		>,
+		BaseInputsProps {
+	ref?: React.Ref<HTMLInputElement>;
+}
